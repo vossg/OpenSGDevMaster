04697894310c4f3a0c3cab81a8fb5853402980a7
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2006-09-18 08:23:18 +0000

    added Texture Obj Ref Chunk
    git-svn-id: https://opensg.vrsource.org/svn/trunk@125 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/System/State/Auxiliary/OSGTextureObjRefChunk.cpp
@@ -0,0 +1,242 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000-2002 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <boost/bind.hpp>
+
+#include "OSGConfig.h"
+
+#include <OSGGL.h>
+#include <OSGGLU.h>
+#include <OSGGLEXT.h>
+#include <OSGImage.h>
+
+#include "OSGDrawActionBase.h"
+#include "OSGDrawEnv.h"
+
+#include "OSGTextureObjRefChunk.h"
+
+//#define OSG_DUMP_TEX
+
+OSG_USING_NAMESPACE
+
+
+/***************************************************************************\
+ *                            Description                                  *
+\***************************************************************************/
+
+/*! \class osg::TextureObjRefChunk
+    \ingroup GrpSystemState
+
+*/
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                -
+\*-------------------------------------------------------------------------*/
+
+void TextureObjRefChunk::initMethod(InitPhase ePhase)
+{
+    Inherited::initMethod(ePhase);
+}
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                 -
+\*-------------------------------------------------------------------------*/
+
+
+/*------------- constructors & destructors --------------------------------*/
+
+TextureObjRefChunk::TextureObjRefChunk(void) :
+    Inherited()
+{
+}
+
+TextureObjRefChunk::TextureObjRefChunk(const TextureObjRefChunk &source) :
+    Inherited(source)
+{
+}
+
+TextureObjRefChunk::~TextureObjRefChunk(void)
+{
+}
+
+/*------------------------- Chunk Class Access ---------------------------*/
+
+/*------------------------------- Sync -----------------------------------*/
+
+/*! React to field changes.
+    Note: this function also handles CubeTexture changes, make sure to keep
+    it consistent with the cubeTexture specifics
+*/
+
+void TextureObjRefChunk::changed(BitVector whichField, UInt32 origin)
+{
+    Inherited::changed(whichField, origin);
+}
+
+bool TextureObjRefChunk::isTransparent(void) const
+{
+    // Even if the texture has alpha, the Blending is makes the sorting
+    // important, thus textures per se are not transparent
+    return false;
+}
+
+
+/*----------------------------- onCreate --------------------------------*/
+
+void TextureObjRefChunk::onCreate(const TextureObjRefChunk *source)
+{
+    Inherited::onCreate(source);
+}
+
+void TextureObjRefChunk::onCreateAspect(const TextureObjRefChunk *createAspect,
+                                        const TextureObjRefChunk *source      )
+{
+    Inherited::onCreateAspect(createAspect, source);
+}
+
+/*------------------------------ Output ----------------------------------*/
+
+void TextureObjRefChunk::dump(      UInt32    OSG_CHECK_ARG(uiIndent),
+                              const BitVector OSG_CHECK_ARG(bvFlags )) const
+{
+    SLOG << "Dump TextureObjRefChunk NI" << std::endl;
+}
+
+
+/*------------------------------ State ------------------------------------*/
+
+
+void TextureObjRefChunk::activate(DrawEnv *pEnv, UInt32 idx)
+{    
+    glBindTexture(this->getTarget(), 
+                  this->getGLId  ());
+
+
+    glEnable(this->getTarget());
+}
+
+
+void TextureObjRefChunk::changeFrom(DrawEnv    *pEnv,
+                                    StateChunk *old   ,
+                                    UInt32      idx )
+{
+    // change from me to me?
+    // this assumes I haven't changed in the meantime.
+    // is that a valid assumption?
+    if(old == this)
+        return;
+
+    glBindTexture(this->getTarget(), 
+                  this->getGLId  ());
+
+    glEnable(this->getTarget());
+}
+
+void TextureObjRefChunk::deactivate(DrawEnv *pEnv, UInt32 idx)
+{
+    glDisable(this->getTarget());
+}
+
+/*-------------------------- Comparison -----------------------------------*/
+
+Real32 TextureObjRefChunk::switchCost(StateChunk *OSG_CHECK_ARG(chunk))
+{
+    return 0;
+}
+
+bool TextureObjRefChunk::operator < (const StateChunk &other) const
+{
+    return this < &other;
+}
+
+bool TextureObjRefChunk::operator == (const StateChunk &other) const
+{
+    bool returnValue = false;
+
+    return returnValue;
+}
+
+bool TextureObjRefChunk::operator != (const StateChunk &other) const
+{
+    return ! (*this == other);
+}
+
+
+/*------------------------------------------------------------------------*/
+/*                              cvs id's                                  */
+
+#ifdef OSG_SGI_CC
+#pragma set woff 1174
+#endif
+
+#ifdef OSG_LINUX_ICC
+#pragma warning( disable : 177 )
+#endif
+
+namespace
+{
+    static Char8 cvsid_cpp       [] = "@(#)$Id: $";
+    static Char8 cvsid_hpp       [] = OSGTEXTUREOBJREFCHUNK_HEADER_CVSID;
+    static Char8 cvsid_inl       [] = OSGTEXTUREOBJREFCHUNK_INLINE_CVSID;
+
+    static Char8 cvsid_fields_hpp[] = OSGTEXTUREOBJREFCHUNKFIELDS_HEADER_CVSID;
+}
+
+#ifdef __sgi
+#pragma reset woff 1174
+#endif
+
---
+++ Source/System/State/Auxiliary/OSGTextureObjRefChunk.h
@@ -0,0 +1,209 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000-2002 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zghdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGTEXTUREOBJREFCHUNK_H_
+#define _OSGTEXTUREOBJREFCHUNK_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#include "OSGConfig.h"
+#include "OSGGLEXT.h"
+#include "OSGWindow.h"
+#include "OSGImage.h"
+#include "OSGTextureObjRefChunkBase.h"
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief State chunk for textures. See \ref PageSystemTextureObjRefChunk 
+    for a description.
+ */
+
+class OSG_SYSTEM_DLLMAPPING TextureObjRefChunk : public TextureObjRefChunkBase
+{
+    /*==========================  PUBLIC  =================================*/
+
+  public:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                 Chunk Class Access                           */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name              Static Chunk Class Access                       */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                    Chunk Id                                  */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Sync                                    */
+    /*! \{                                                                 */
+
+    virtual void changed(ConstFieldMaskArg whichField,
+                         UInt32            origin    );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Output                                  */
+    /*! \{                                                                 */
+
+    virtual void dump(      UInt32    uiIndent = 0,
+                      const BitVector bvFlags  = 0) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                       State                                  */
+    /*! \{                                                                 */
+
+    virtual void activate   (DrawEnv    *pEnv, 
+                             UInt32      index = 0);
+
+    virtual void changeFrom (DrawEnv    *pEnv, 
+                             StateChunk *pOld,
+                             UInt32      index = 0);
+
+    virtual void deactivate (DrawEnv    *pEnv, 
+                             UInt32      index = 0);
+
+    virtual bool isTransparent (void) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                       query                                  */
+    /*! \{                                                                 */
+
+    GLenum determineTextureTarget(Window *pWindow) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Comparison                                 */
+    /*! \{                                                                 */
+
+    virtual Real32 switchCost(StateChunk * chunk);
+
+    virtual bool   operator <  (const StateChunk &other) const;
+
+    virtual bool   operator == (const StateChunk &other) const;
+    virtual bool   operator != (const StateChunk &other) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                Texture specific                              */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name             Multitexture handling                            */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */
+    /*=========================  PROTECTED  ===============================*/
+
+  protected:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                       Init                                   */
+    /*! \{                                                                 */
+
+    void onCreate      (const TextureObjRefChunk *source      = NULL);
+    void onCreateAspect(const TextureObjRefChunk *createAspect,
+                        const TextureObjRefChunk *source      = NULL);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Constructors                               */
+    /*! \{                                                                 */
+
+    TextureObjRefChunk(void);
+    TextureObjRefChunk(const TextureObjRefChunk &source);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructors                                */
+    /*! \{                                                                 */
+
+    virtual ~TextureObjRefChunk(void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                       Init                                   */
+    /*! \{                                                                 */
+
+    static void initMethod(InitPhase ePhase);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                         GL                                   */
+    /*! \{                                                                 */
+
+    /*! \}                                                                 */ 
+    /*---------------------------------------------------------------------*/
+
+    /*==========================  PRIVATE  ================================*/
+
+  private:
+
+    typedef TextureObjRefChunkBase Inherited;
+
+    friend class TextureObjRefChunkBase;
+
+    template<class ContainerFactoryT>
+    friend struct CPtrConstructionFunctions;
+
+    template<class ContainerFactoryT>
+    friend struct PtrConstructionFunctions;
+
+    // prohibit default functions (move to 'public' if you need one)
+    void operator =(const TextureObjRefChunk &source);
+};
+
+typedef TextureObjRefChunk *TextureObjRefChunkP;
+
+OSG_END_NAMESPACE
+
+#include "OSGTextureObjRefChunkBase.inl"
+#include "OSGTextureObjRefChunk.inl"
+
+#define OSGTEXTUREOBJREFCHUNK_HEADER_CVSID "@(#)$Id: $"
+
+#endif /* _OSGTEXTUREOBJREFCHUNK_H_ */
