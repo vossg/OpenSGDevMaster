c92d708189215bd476939c6bba7cac4a7a186316
allenb <allenb@4683daeb-ad0f-0410-a623-93161e962ae5>
2006-09-25 15:46:04 +0000

    svn tags??
    git-svn-id: https://opensg.vrsource.org/svn/trunk@164 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/State/OpenGL/OSGCubeTextureObjChunkBase.cpp
+++ Source/System/State/OpenGL/OSGCubeTextureObjChunkBase.cpp
@@ -628,7 +628,7 @@ OSG_END_NAMESPACE
 
 namespace
 {
-    static Char8 cvsid_cpp       [] = "@(#)$Id: $";
+    static Char8 cvsid_cpp       [] = "@(#)$Id: FCBaseTemplate_cpp.h 120 2006-09-18 08:12:52Z vossg $";
     static Char8 cvsid_hpp       [] = OSGCUBETEXTUREOBJCHUNKBASE_HEADER_CVSID;
     static Char8 cvsid_inl       [] = OSGCUBETEXTUREOBJCHUNKBASE_INLINE_CVSID;
 
