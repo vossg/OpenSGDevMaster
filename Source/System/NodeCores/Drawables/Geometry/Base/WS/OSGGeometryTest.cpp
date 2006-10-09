1fbb17201555acaae96619965989f8428b0275a6
allenb <allenb@4683daeb-ad0f-0410-a623-93161e962ae5>
2006-10-09 13:18:11 +0000

    Added unit test for geometry.  This still needs more work, but it does stress the system a bit.
    git-svn-id: https://opensg.vrsource.org/svn/trunk@256 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/System/NodeCores/Drawables/Geometry/Base/WS/OSGGeometryTest.cpp
@@ -0,0 +1,210 @@
+#include <UnitTest++.h>
+
+// Unit tests for vec classes
+
+#include <OpenSG/OSGGeometry.h>
+#include <OpenSG/OSGGeoVectorProperty.h>
+#include <OpenSG/OSGGeoProperties.h>
+#include <OpenSG/OSGFieldContainerFactory.h>
+#include <OpenSG/OSGTypedGeoVectorProperty.h>
+
+
+TEST(CreateGeometry)
+{
+   OSG::GeometryPtr g = OSG::Geometry::create();
+   CHECK(g != OSG::NullFC);
+}
+
+TEST(TestInitialValues)
+{
+   OSG::GeometryPtr g = OSG::Geometry::create();
+   CHECK(g != OSG::NullFC);
+
+   for(unsigned i=0;i<OSG::Geometry::MaxAttribs;i++)
+   {
+      CHECK(g->getProperty(i) == OSG::NullFC);
+      CHECK(g->getIndex(i) == OSG::NullFC);
+   }
+
+   CHECK(g->getMaterial() == OSG::NullFC);
+}
+
+// Test using the generic vector interfaces on geometry
+TEST(TestGenericInterfaces)
+{
+   OSG::GeometryPtr g = OSG::Geometry::create();
+   CHECK(g != OSG::NullFC);
+
+   OSG::GeoPnt3fPropertyPtr  pnts  = OSG::GeoPnt3fProperty ::create();
+   pnts->addValue(OSG::Pnt3f(0,0,0));
+
+   g->setPositions(pnts);
+
+   OSG::GeoVectorPropertyPtr pnts_g = g->getPositions();
+   pnts_g->setValue(OSG::Pnt3f(1,1,1), 0);
+   pnts_g->setValue(OSG::Pnt3s(1,1,1), 0);
+   pnts_g->setValue(OSG::Pnt2f(1,1  ), 0);
+   pnts_g->setValue(OSG::Vec3f(1,1,1), 0);
+   pnts_g->setValue(OSG::Vec2f(1,2  ), 0);
+   pnts_g->setValue(OSG::Vec4f(1,2,3,4), 0);
+   pnts_g->setValue(OSG::Color3f(1,2,3), 0);
+   pnts_g->setValue(OSG::Color4f(1,2,3,4), 0);
+}
+
+
+TEST(TestSettingVecAttribs)
+{
+   // Test setting attributes to all possible value types
+   OSG::GeometryPtr g = OSG::Geometry::create();
+   CHECK(g != OSG::NullFC);
+
+   // Copied-and-macroed from OSGTypedGeoVectorPropertyFields
+   char* allowed_vec_prop_types[] =
+   {
+"GeoVec1ubProperty",
+"GeoVec2ubProperty",
+"GeoVec3ubProperty",
+"GeoVec4ubProperty",
+
+"GeoPnt1ubProperty",
+"GeoPnt2ubProperty",
+"GeoPnt3ubProperty",
+"GeoPnt4ubProperty",
+
+"GeoVec1NubProperty",
+"GeoVec2NubProperty",
+"GeoVec3NubProperty",
+"GeoVec4NubProperty",
+
+"GeoPnt1NubProperty",
+"GeoPnt2NubProperty",
+"GeoPnt3NubProperty",
+"GeoPnt4NubProperty",
+
+
+"GeoVec1bProperty",
+"GeoVec2bProperty",
+"GeoVec3bProperty",
+"GeoVec4bProperty",
+
+"GeoPnt1bProperty",
+"GeoPnt2bProperty",
+"GeoPnt3bProperty",
+"GeoPnt4bProperty",
+
+"GeoVec1NbProperty",
+"GeoVec2NbProperty",
+"GeoVec3NbProperty",
+"GeoVec4NbProperty",
+
+"GeoPnt1NbProperty",
+"GeoPnt2NbProperty",
+"GeoPnt3NbProperty",
+"GeoPnt4NbProperty",
+
+
+"GeoVec1usProperty",
+"GeoVec2usProperty",
+"GeoVec3usProperty",
+"GeoVec4usProperty",
+
+"GeoPnt1usProperty",
+"GeoPnt2usProperty",
+"GeoPnt3usProperty",
+"GeoPnt4usProperty",
+
+"GeoVec1NusProperty",
+"GeoVec2NusProperty",
+"GeoVec3NusProperty",
+"GeoVec4NusProperty",
+
+"GeoPnt1NusProperty",
+"GeoPnt2NusProperty",
+"GeoPnt3NusProperty",
+"GeoPnt4NusProperty",
+
+
+"GeoVec1sProperty",
+"GeoVec2sProperty",
+"GeoVec3sProperty",
+"GeoVec4sProperty",
+
+"GeoPnt1sProperty",
+"GeoPnt2sProperty",
+"GeoPnt3sProperty",
+"GeoPnt4sProperty",
+
+"GeoVec1NsProperty",
+"GeoVec2NsProperty",
+"GeoVec3NsProperty",
+"GeoVec4NsProperty",
+
+"GeoPnt1NsProperty",
+"GeoPnt2NsProperty",
+"GeoPnt3NsProperty",
+"GeoPnt4NsProperty",
+
+#ifndef OSG_WINCE
+"GeoVec1fProperty",
+"GeoVec2fProperty",
+"GeoVec3fProperty",
+"GeoVec4fProperty",
+
+"GeoPnt1fProperty",
+"GeoPnt2fProperty",
+"GeoPnt3fProperty",
+"GeoPnt4fProperty",
+#endif
+
+#ifdef OSG_WINCE
+"GeoVec1fxProperty",
+"GeoVec2fxProperty",
+"GeoVec3fxProperty",
+"GeoVec4fxProperty",
+
+"GeoPnt1fxProperty",
+"GeoPnt2fxProperty",
+"GeoPnt3fxProperty",
+"GeoPnt4fxProperty",
+#endif
+
+#ifndef OSG_WINCE
+"GeoVec1dProperty",
+"GeoVec2dProperty",
+"GeoVec3dProperty",
+"GeoVec4dProperty",
+
+"GeoPnt1dProperty",
+"GeoPnt2dProperty",
+"GeoPnt3dProperty",
+"GeoPnt4dProperty",
+#endif
+
+"GeoColor3ubProperty",
+"GeoColor4ubProperty",
+"GeoColor3NubProperty",
+"GeoColor4NubProperty",
+
+"GeoColor3fProperty",
+"GeoColor4fProperty"
+   };
+
+   OSG::GeoVectorPropertyPtr att;
+
+   unsigned num_vec_props = sizeof(allowed_vec_prop_types)/sizeof(char*);
+
+   for(unsigned i=0; i<num_vec_props;++i)
+   {
+      att = OSG::cast_dynamic<OSG::GeoVectorPropertyPtr>(
+                OSG::FieldContainerFactory::the()->createContainer(
+                    allowed_vec_prop_types[i]));
+      unsigned prop_index = i%16;
+      g->setProperty(att,prop_index);
+      CHECK(true);
+   }
+
+}
+
+
+
+
