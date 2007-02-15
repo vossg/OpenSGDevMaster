2cd0154b722a7b13af91654b563e59471bb002af
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2007-02-15 07:19:51 +0000

    fixed : win compile problems
    git-svn-id: https://opensg.vrsource.org/svn/trunk@540 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/Cluster/Base/OSGClusterDef.h
+++ Source/System/Cluster/Base/OSGClusterDef.h
@@ -46,7 +46,7 @@
 //  Defines
 //---------------------------------------------------------------------------
 
-#if defined(WIN32) && defined(OSG_BUILD_DLL)
+#if defined(WIN32)
 #   ifdef OSG_COMPILECLUSTERLIB
 #       define OSG_CLUSTER_DLLMAPPING  __declspec(dllexport)
 #       define OSG_CLUSTER_EXPIMP_TMPL 
