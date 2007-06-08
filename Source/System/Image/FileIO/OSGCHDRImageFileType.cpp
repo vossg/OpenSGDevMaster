0a94d55dc6bed2c502b3ae90282f6191193d11bf
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2007-06-08 08:09:19 +0000

    hack : auto convert hdr cross images to cubemaps
    git-svn-id: https://opensg.vrsource.org/svn/trunk@762 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/System/Image/FileIO/OSGCHDRImageFileType.cpp
@@ -0,0 +1,248 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000-2002 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//-------------------------------
+//      Includes                                    
+//-------------------------------
+
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+#include <memory.h>
+
+#include <OSGConfig.h>
+
+#include <iostream>
+#include <fstream>
+
+#include <OSGLog.h>
+#include <OSGImageFileHandler.h>
+#include <OSGPathHandler.h>
+#include <OSGFileSystem.h>
+#include <OSGImageFunctions.h>
+
+#include "OSGCHDRImageFileType.h"
+
+OSG_USING_NAMESPACE
+
+
+/*! \class OSG::HDRImageFileType 
+    \ingroup GrpSystemImage
+
+Image File Type to read/write and store/restore Image objects as
+HDR data.
+
+All the type specific code is included in the class. Does
+not depend on external libs.
+    
+ */
+
+/*****************************
+ *   Types
+ *****************************/
+// Static Class Varible implementations: 
+
+static const Char8 *suffixArray[] = 
+{
+    "chdr"
+};
+
+CHDRImageFileType CHDRImageFileType::_the("chdr",
+                                          suffixArray, sizeof(suffixArray),
+                                          OSG_READ_SUPPORTED);
+
+
+/*****************************
+ *    Classvariables
+ *****************************/
+
+
+/********************************
+ *    Class methodes
+ *******************************/
+
+
+/*******************************
+*public
+*******************************/
+
+//-------------------------------------------------------------------------
+/*!
+  Tries to fill the image object with the data read from
+  the given input stream. Returns true on success.
+*/
+
+bool CHDRImageFileType::read(      ImagePtrArg   image, 
+                                   std::istream &is, 
+                             const std::string  &mimetype)
+{
+    ImagePtr pTmpImage = Image::create();
+
+    bool returnValue   = Inherited::read(pTmpImage, is, mimetype);
+
+    if(returnValue == false)
+    {
+        OSG::subRef(pTmpImage);
+
+        return returnValue;
+    }
+
+
+    returnValue = convertCrossToCubeMap(pTmpImage,
+                                        image);
+
+    subRef(pTmpImage);
+
+    return returnValue;
+}
+
+//-------------------------------------------------------------------------
+/*!
+  Tries to write the image object to the given output stream.
+  Returns true on success.
+*/
+
+bool CHDRImageFileType::write(      ImageConstPtrArg  image, 
+                                    std::ostream     &os, 
+                              const std::string      &mimetype)
+{
+#if 0
+    if(image->getDataType() != Image::OSG_FLOAT32_IMAGEDATA)
+    {
+        FWARNING(("HDRImageFileType::write: Image has non float data "
+                  "type!\n"));
+        return false;
+    }
+    
+    if(!os.good())
+        return false;
+
+    int width  = image->getWidth();
+    int height = image->getHeight();
+
+    os << "#?RADIANCE" << std::endl;
+    os << "# Written with OpenSG" << std::endl;
+    os << "FORMAT=32-bit_rle_rgbe" << std::endl;
+    os << "EXPOSURE=" << 1.0f << std::endl << std::endl;
+    os << "-Y " << height << " +X " << width << std::endl;
+
+    RGBE *rgbe_scan = new RGBE[width];
+
+    Real32 *data = ((Real32 *)(image->getData()));
+
+    //upside down !!!
+    for(int y=height-1;y>=0;y--)
+    {
+        if (fwritecolrs(os, 
+                        &data[y * width * 3], 
+                        rgbe_scan, 
+                        width, 
+                        height) < 0)
+        {
+            delete [] rgbe_scan;
+            return false;
+        }
+    }
+
+    delete [] rgbe_scan;
+#endif
+
+    return true;
+}
+
+//-------------------------------------------------------------------------
+/*!
+  Tries to restore the image data from the given memblock.
+  Returns the amount of data read.
+*/
+
+UInt64 CHDRImageFileType::restoreData(      ImagePtrArg  image, 
+                                      const UChar8      *buffer,
+                                            Int32              )
+{
+    image->setData(buffer);
+
+    return image->getSize();
+}
+
+//-------------------------------------------------------------------------
+/*!
+  Tries to store the image data to the given memblock.
+  Returns the amount of data written.
+*/
+
+UInt64 CHDRImageFileType::storeData(ImageConstPtrArg  image, 
+                                    UChar8           *buffer,
+                                    Int32             OSG_CHECK_ARG(memSize))
+{
+    UInt32 dataSize = image->getSize();
+
+    const UChar8 *src = image->getData();
+
+    if(dataSize && src && buffer)
+        memcpy(buffer, src, dataSize);
+  
+    return dataSize;
+} 
+
+
+//-------------------------------------------------------------------------
+/*!
+  Constructor used for the singleton object
+*/
+
+CHDRImageFileType::CHDRImageFileType(const Char8 *mimeType,
+                                     const Char8 *suffixArray[], 
+                                           UInt16 suffixByteCount,
+                                           UInt32 flags          ) :
+    Inherited(mimeType, 
+              suffixArray, 
+              suffixByteCount, 
+              flags )
+{
+}
+
+//-------------------------------------------------------------------------
+/*!
+  Destructor
+*/
+
+CHDRImageFileType::~CHDRImageFileType(void)
+{
+}
+
---
+++ Source/System/Image/FileIO/OSGCHDRImageFileType.h
@@ -0,0 +1,131 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *             Copyright (C) 2000-2002 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef OSGCHDRIMAGEFILETYPE
+#define OSGCHDRIMAGEFILETYPE
+
+#ifdef  __sgi
+#pragma  once
+#endif
+
+#include "OSGHDRImageFileType.h"
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief DAT File Handler. Used to read/write DAT/RAW files.
+See \ref PageSystemImage for a detailed description.
+*/
+
+class OSG_IMGFILEIO_DLLMAPPING CHDRImageFileType : public HDRImageFileType
+{
+    /*==========================  PUBLIC  =================================*/
+
+  public:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructor                                 */
+    /*! \{                                                                 */
+
+    virtual ~CHDRImageFileType (void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Read/Write                                 */
+    /*! \{                                                                 */
+
+    virtual bool read (      ImagePtrArg       image,
+                             std::istream     &is, 
+                       const std::string      &mimeType);
+
+    virtual bool write(      ImageConstPtrArg  image,
+                             std::ostream     &os, 
+                       const std::string      &mimeType);
+
+    /*! \}                                                                 */
+    /*=========================  PROTECTED  ===============================*/
+
+  protected:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name               Default Constructor                            */
+    /*! \{                                                                 */
+
+    CHDRImageFileType(const Char8 *mimeType,
+                      const Char8 *suffixArray[], 
+                            UInt16  suffixByteCount,
+                            UInt32  flags );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                     Buffer                                   */
+    /*! \{                                                                 */
+
+    virtual UInt64 restoreData(      ImagePtrArg       image, 
+                               const UChar8           *buffer,
+                                     Int32             memSize = -1);
+
+    virtual UInt64 storeData  (      ImageConstPtrArg  image, 
+                                     UChar8           *buffer,
+                                     Int32             memSize = -1);
+    /*! \}                                                                 */
+
+    /*==========================  PRIVATE  ================================*/
+
+  private:
+
+    typedef HDRImageFileType Inherited;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                       Copy Op                                */
+    /*! \{                                                                 */
+
+          CHDRImageFileType            (const CHDRImageFileType &obj);
+    const CHDRImageFileType &operator =(const CHDRImageFileType &obj);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+
+    static  CHDRImageFileType _the;
+};
+
+typedef CHDRImageFileType *CHDRImageFileTypeP;
+
+OSG_END_NAMESPACE
+
+#endif // OSGCHDRIMAGEFILETYPE
