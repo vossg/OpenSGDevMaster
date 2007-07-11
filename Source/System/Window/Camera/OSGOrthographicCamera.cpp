5c5213f7e033caef7071c12036fbaae98e410124
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2007-07-11 03:23:31 +0000

    added ortho camera from 1.x
    git-svn-id: https://opensg.vrsource.org/svn/trunk@832 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/System/Window/Camera/OSGOrthographicCamera.cpp
@@ -0,0 +1,136 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2002 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <OSGConfig.h>
+#include <OSGMatrixUtility.h>
+
+#include "OSGOrthographicCamera.h"
+
+OSG_USING_NAMESPACE
+
+/***************************************************************************\
+ *                            Description                                  *
+\***************************************************************************/
+
+/*! \class osg::OrthographicCamera
+An orthographic camera.
+*/
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+void OrthographicCamera::initMethod(InitPhase ePhase)
+{
+    Inherited::initMethod(ePhase);
+}
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*----------------------- constructors & destructors ----------------------*/
+
+OrthographicCamera::OrthographicCamera(void) :
+    Inherited()
+{
+}
+
+OrthographicCamera::OrthographicCamera(const OrthographicCamera &source) :
+    Inherited(source)
+{
+}
+
+OrthographicCamera::~OrthographicCamera(void)
+{
+}
+
+/*----------------------------- class specific ----------------------------*/
+
+void OrthographicCamera::changed(ConstFieldMaskArg whichField, UInt32 origin)
+{
+    Inherited::changed(whichField, origin);
+}
+
+/*-------------------------- your_category---------------------------------*/
+
+void OrthographicCamera::getProjection(Matrixr &result, 
+                                       UInt32   width, 
+                                       UInt32   height)
+{
+    Real32 vs = getVerticalSize();
+
+    // catch some illegal cases
+    if(vs < 0 || width == 0 || height == 0)
+    {
+        result.setIdentity();
+
+        return;
+    }
+
+    Real32 a = width /(Real32) height * getAspect();
+    
+    MatrixOrthogonal(result, -vs / 2 * a,  vs / 2 * a, 
+                             -vs / 2, vs / 2,
+                             getNear(), getFar());
+}
+    
+
+void OrthographicCamera::dump(      UInt32    , 
+                         const BitVector ) const
+{
+    SLOG << "Dump OrthographicCamera NI" << std::endl;
+}
+
+
---
+++ Source/System/Window/Camera/OSGOrthographicCamera.h
@@ -0,0 +1,133 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2002 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGORTHOGRAPHICCAMERA_H_
+#define _OSGORTHOGRAPHICCAMERA_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#include "OSGOrthographicCameraBase.h"
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief OrthographicCamera class. See \ref 
+           PageSystemOrthographicCamera for a description.
+*/
+
+class OSG_SYSTEM_DLLMAPPING OrthographicCamera : public OrthographicCameraBase
+{
+  protected:
+
+    typedef OrthographicCameraBase Inherited;
+
+    /*==========================  PUBLIC  =================================*/
+
+  public:
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Sync                                    */
+    /*! \{                                                                 */
+
+    virtual void changed(ConstFieldMaskArg  whichField, 
+                         UInt32     origin    );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Camera-specific                             */
+    /*! \{                                                                 */
+
+    virtual void getProjection(Matrixr &result, 
+                               UInt32   width, 
+                               UInt32   height);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                     Output                                   */
+    /*! \{                                                                 */
+
+    virtual void dump(      UInt32     uiIndent = 0, 
+                      const BitVector  bvFlags  = 0) const;
+
+    /*! \}                                                                 */
+    /*=========================  PROTECTED  ===============================*/
+  protected:
+
+    // Variables should all be in OrthographicCameraBase.
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Constructors                                */
+    /*! \{                                                                 */
+
+    OrthographicCamera(void);
+    OrthographicCamera(const OrthographicCamera &source);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructors                                */
+    /*! \{                                                                 */
+
+    virtual ~OrthographicCamera(void); 
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                       Init                                   */
+    /*! \{                                                                 */
+
+    static void initMethod(InitPhase ePhase);
+
+    /*! \}                                                                 */
+    /*==========================  PRIVATE  ================================*/
+
+  private:
+
+    friend class FieldContainer;
+    friend class OrthographicCameraBase;
+
+    // prohibit default functions (move to 'public' if you need one)
+    void operator =(const OrthographicCamera &source);
+};
+
+typedef OrthographicCamera *OrthographicCameraP;
+
+OSG_END_NAMESPACE
+
+#include "OSGOrthographicCameraBase.inl"
+#include "OSGOrthographicCamera.inl"
+
+#endif /* _OSGORTHOGRAPHICCAMERA_H_ */
