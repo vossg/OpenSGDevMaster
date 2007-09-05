4c558b8a53f88a0e100ea1bf8c73f5b294d3081c
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2007-09-05 11:31:36 +0000

    fixed: added missing shader pnt parameter classes
    git-svn-id: https://opensg.vrsource.org/svn/trunk@935 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/System/State/SHL/Parameter/OSGShaderParameterPnt2f.cpp
@@ -0,0 +1,115 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2002 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <OSGConfig.h>
+
+#include "OSGShaderParameterPnt2f.h"
+
+OSG_USING_NAMESPACE
+
+// Documentation for this class is emited in the
+// OSGShaderParameterPnt2fBase.cpp file.
+// To modify it, please change the .fcd file (OSGShaderParameterPnt2f.fcd) and
+// regenerate the base file.
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+void ShaderParameterPnt2f::initMethod(InitPhase ePhase)
+{
+    Inherited::initMethod(ePhase);
+}
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*----------------------- constructors & destructors ----------------------*/
+
+ShaderParameterPnt2f::ShaderParameterPnt2f(void) :
+    Inherited()
+{
+    setTypeId(ShaderParameter::SHPTypePnt2f);
+}
+
+ShaderParameterPnt2f::ShaderParameterPnt2f(
+    const ShaderParameterPnt2f &source) :
+
+    Inherited(source)
+{
+}
+
+ShaderParameterPnt2f::~ShaderParameterPnt2f(void)
+{
+}
+
+/*----------------------------- class specific ----------------------------*/
+
+void ShaderParameterPnt2f::changed(ConstFieldMaskArg whichField, 
+                                   UInt32            origin,
+                                   BitVector         details)
+{
+    if(whichField & ShaderParameterPnt2f::ValueFieldMask)
+        setChanged();
+
+    Inherited::changed(whichField, origin, details);
+}
+
+void ShaderParameterPnt2f::dump(      UInt32    , 
+                                const BitVector ) const
+{
+    SLOG << "Dump ShaderParameterPnt2f NI" << std::endl;
+}
+
---
+++ Source/System/State/SHL/Parameter/OSGShaderParameterPnt3f.cpp
@@ -0,0 +1,115 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2002 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <OSGConfig.h>
+
+#include "OSGShaderParameterPnt3f.h"
+
+OSG_USING_NAMESPACE
+
+// Documentation for this class is emited in the
+// OSGShaderParameterPnt3fBase.cpp file.
+// To modify it, please change the .fcd file (OSGShaderParameterPnt3f.fcd) and
+// regenerate the base file.
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+void ShaderParameterPnt3f::initMethod(InitPhase ePhase)
+{
+    Inherited::initMethod(ePhase);
+}
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*----------------------- constructors & destructors ----------------------*/
+
+ShaderParameterPnt3f::ShaderParameterPnt3f(void) :
+    Inherited()
+{
+    setTypeId(ShaderParameter::SHPTypePnt3f);
+}
+
+ShaderParameterPnt3f::ShaderParameterPnt3f(
+    const ShaderParameterPnt3f &source) :
+    
+    Inherited(source)
+{
+}
+
+ShaderParameterPnt3f::~ShaderParameterPnt3f(void)
+{
+}
+
+/*----------------------------- class specific ----------------------------*/
+
+void ShaderParameterPnt3f::changed(ConstFieldMaskArg whichField, 
+                                   UInt32            origin,
+                                   BitVector         details)
+{
+    if(whichField & ShaderParameterPnt3f::ValueFieldMask)
+        setChanged();
+
+    Inherited::changed(whichField, origin, details);
+}
+
+void ShaderParameterPnt3f::dump(      UInt32    , 
+                                const BitVector ) const
+{
+    SLOG << "Dump ShaderParameterPnt3f NI" << std::endl;
+}
+
