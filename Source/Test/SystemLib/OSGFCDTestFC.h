e01f60ff91c72e48e4b81df0c1dee063cd2b0574
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2008-04-04 06:06:02 +0000

    changed: added fcd field gen test
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1115 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/Test/SystemLib/OSGFCDTestFC.h
@@ -0,0 +1,127 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2006 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGFCDTESTFC_H_
+#define _OSGFCDTESTFC_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#include "OSGFCDTestFCBase.h"
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief FCDTestFC class. See \ref
+           PageSystemFCDTestFC for a description.
+*/
+
+class OSG_SYSTEM_DLLMAPPING FCDTestFC : public FCDTestFCBase
+{
+  protected:
+
+    /*==========================  PUBLIC  =================================*/
+
+  public:
+
+    typedef FCDTestFCBase Inherited;
+    typedef FCDTestFC     Self;
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Sync                                    */
+    /*! \{                                                                 */
+
+    virtual void changed(ConstFieldMaskArg whichField,
+                         UInt32            origin,
+                         BitVector         details    );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                     Output                                   */
+    /*! \{                                                                 */
+
+    virtual void dump(      UInt32     uiIndent = 0,
+                      const BitVector  bvFlags  = 0) const;
+
+    /*! \}                                                                 */
+    /*=========================  PROTECTED  ===============================*/
+
+  protected:
+
+    // Variables should all be in FCDTestFCBase.
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Constructors                                */
+    /*! \{                                                                 */
+
+    FCDTestFC(void);
+    FCDTestFC(const FCDTestFC &source);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructors                                */
+    /*! \{                                                                 */
+
+    virtual ~FCDTestFC(void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Init                                    */
+    /*! \{                                                                 */
+
+    static void initMethod(InitPhase ePhase);
+
+    /*! \}                                                                 */
+    /*==========================  PRIVATE  ================================*/
+
+  private:
+
+    friend class FieldContainer;
+    friend class FCDTestFCBase;
+
+    // prohibit default functions (move to 'public' if you need one)
+    void operator =(const FCDTestFC &source);
+};
+
+typedef FCDTestFC *FCDTestFCP;
+
+OSG_END_NAMESPACE
+
+#include "OSGFCDTestFCBase.inl"
+#include "OSGFCDTestFC.inl"
+
+#endif /* _OSGFCDTESTFC_H_ */
