7dc10eeb51a08d0831b43070506776ef21129f96
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2008-07-29 05:31:18 +0000

    changed: updated to a later flex version (2.5.35) to fix mem leaks
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1395 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/FileIO/ScanParseSkel/prebuild/OSGScanParseSkelScanner.cpp
+++ Source/System/FileIO/ScanParseSkel/prebuild/OSGScanParseSkelScanner.cpp
@@ -8,7 +8,7 @@
 #define FLEX_SCANNER
 #define YY_FLEX_MAJOR_VERSION 2
 #define YY_FLEX_MINOR_VERSION 5
-#define YY_FLEX_SUBMINOR_VERSION 33
+#define YY_FLEX_SUBMINOR_VERSION 35
 #if YY_FLEX_SUBMINOR_VERSION > 0
 #define FLEX_BETA
 #endif
@@ -34,7 +34,7 @@
 
 /* C99 systems have <inttypes.h>. Non-C99 systems may or may not. */
 
-#if __STDC_VERSION__ >= 199901L
+#if defined (__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
 
 /* C99 says to define __STDC_LIMIT_MACROS before including stdint.h,
  * if you want the limit (max/min) macros for int types. 
@@ -104,11 +104,12 @@ typedef unsigned int flex_uint32_t;
 
 #else	/* ! __cplusplus */
 
-#if __STDC__
+/* C99 requires __STDC__ to be defined as 1. */
+#if defined (__STDC__)
 
 #define YY_USE_CONST
 
-#endif	/* __STDC__ */
+#endif	/* defined (__STDC__) */
 #endif	/* ! __cplusplus */
 
 #ifdef YY_USE_CONST
@@ -199,14 +200,9 @@ extern int yyleng;
 
 #define unput(c) yyunput( c, (yytext_ptr)  )
 
-/* The following is because we cannot portably get our hands on size_t
- * (without autoconf's help, which isn't available because we want
- * flex-generated scanners to compile on their own).
- */
-
 #ifndef YY_TYPEDEF_YY_SIZE_T
 #define YY_TYPEDEF_YY_SIZE_T
-typedef unsigned int yy_size_t;
+typedef size_t yy_size_t;
 #endif
 
 #ifndef YY_STRUCT_YY_BUFFER_STATE
@@ -319,8 +315,6 @@ void OSGScanParseSkel_free (void *  );
 #define YY_AT_BOL() (YY_CURRENT_BUFFER_LVALUE->yy_at_bol)
 
 /* Begin user sect3 */
-
-#define yywrap(n) 1
 #define YY_SKIP_YYWRAP
 
 typedef unsigned char YY_CHAR;
@@ -329,7 +323,14 @@ typedef unsigned char YY_CHAR;
 #define YY_INTERACTIVE
 
 //#include <FlexLexer.h>
-#include <OSGScanParseSkelScanner_FlexLexer.h>
+#include "OSGConfig.h"
+#ifdef OSG_PREBUILD_SCANPARSE
+#include "OSGScanParseSkelScanner_FlexLexer.h"
+#else
+#include "FlexLexer.h"
+#endif
+
+int yyFlexLexer::yywrap() { return 1; }
 int yyFlexLexer::yylex()
 	{
 	LexerError( "yyFlexLexer::yylex invoked but %option yyclass used" );
@@ -757,7 +758,7 @@ static yyconst flex_int32_t yy_rule_can_match_eol[107] =
 
 /* Not VRML conformant */
 /* Not VRML conformant */
-#line 760 "<stdout>"
+#line 756 "<stdout>"
 
 #define INITIAL 0
 #define NODE 1
@@ -873,7 +874,7 @@ YY_DECL
     }
 
 
-#line 876 "<stdout>"
+#line 872 "<stdout>"
 
 	if ( !(yy_init) )
 		{
@@ -1528,7 +1529,7 @@ YY_RULE_SETUP
 #line 239 "Source/System/FileIO/ScanParseSkel/OSGScanParseSkelScanner.ll"
 ECHO;
 	YY_BREAK
-#line 1531 "<stdout>"
+#line 1527 "<stdout>"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(NODE):
 case YY_STATE_EOF(HEADER):
@@ -1664,6 +1665,8 @@ case YY_STATE_EOF(COMMENT):
 		} /* end of scanning one token */
 } /* end of yylex */
 
+/* The contents of this function are C++ specific, so the () macro is not used.
+ */
 yyFlexLexer::yyFlexLexer( std::istream* arg_yyin, std::ostream* arg_yyout )
 {
 	yyin = arg_yyin;
@@ -1684,21 +1687,26 @@ yyFlexLexer::yyFlexLexer( std::istream* arg_yyin, std::ostream* arg_yyout )
 	yy_start_stack_ptr = yy_start_stack_depth = 0;
 	yy_start_stack = NULL;
 
-    (yy_buffer_stack) = 0;
-    (yy_buffer_stack_top) = 0;
-    (yy_buffer_stack_max) = 0;
+	yy_buffer_stack = 0;
+	yy_buffer_stack_top = 0;
+	yy_buffer_stack_max = 0;
 
 	yy_state_buf = 0;
 
 }
 
+/* The contents of this function are C++ specific, so the () macro is not used.
+ */
 yyFlexLexer::~yyFlexLexer()
 {
 	delete [] yy_state_buf;
 	OSGScanParseSkel_free(yy_start_stack  );
 	yy_delete_buffer( YY_CURRENT_BUFFER );
+	OSGScanParseSkel_free(yy_buffer_stack  );
 }
 
+/* The contents of this function are C++ specific, so the () macro is not used.
+ */
 void yyFlexLexer::switch_streams( std::istream* new_in, std::ostream* new_out )
 {
 	if ( new_in )
@@ -1844,7 +1852,7 @@ int yyFlexLexer::yy_get_next_buffer()
 
 		/* Read in more data. */
 		YY_INPUT( (&YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move]),
-			(yy_n_chars), num_to_read );
+                  (yy_n_chars), size_t(num_to_read) );
 
 		YY_CURRENT_BUFFER_LVALUE->yy_n_chars = (yy_n_chars);
 		}
@@ -1867,6 +1875,14 @@ int yyFlexLexer::yy_get_next_buffer()
 
 	else
 		ret_val = EOB_ACT_CONTINUE_SCAN;
+
+	if (yy_size_t((yy_n_chars) + number_to_move) > YY_CURRENT_BUFFER_LVALUE->yy_buf_size) {
+		/* Extend the array by 50%, plus the number we really need. */
+		yy_size_t new_size = (yy_n_chars) + number_to_move + ((yy_n_chars) >> 1);
+		YY_CURRENT_BUFFER_LVALUE->yy_ch_buf = static_cast<char *>(OSGScanParseSkel_realloc(static_cast<void *>(YY_CURRENT_BUFFER_LVALUE->yy_ch_buf),new_size  ));
+		if ( ! YY_CURRENT_BUFFER_LVALUE->yy_ch_buf )
+			YY_FATAL_ERROR( "out of dynamic memory in yy_get_next_buffer()" );
+	}
 
 	(yy_n_chars) += number_to_move;
 	YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[(yy_n_chars)] = YY_END_OF_BUFFER_CHAR;
@@ -2152,9 +2168,9 @@ int yyFlexLexer::yy_get_next_buffer()
 		YY_CURRENT_BUFFER_LVALUE = YY_BUFFER_STATE(0);
 
 	if ( b->yy_is_our_buffer )
-		OSGScanParseSkel_free(static_cast<void *>(b->yy_ch_buf ) );
+		OSGScanParseSkel_free(static_cast<void *>(b->yy_ch_buf)  );
 
-	OSGScanParseSkel_free(static_cast<void *>( b ) );
+	OSGScanParseSkel_free(static_cast<void *>(b)  );
 }
 
 extern "C" int isatty (int );
@@ -2281,8 +2297,10 @@ void yyFlexLexer::yyensure_buffer_stack(void)
 		num_to_alloc = 1;
 		(yy_buffer_stack) = static_cast<struct yy_buffer_state**>(OSGScanParseSkel_alloc
 								(num_to_alloc * sizeof(struct yy_buffer_state*)
-                                ));
-		
+                                 ));
+		if ( ! (yy_buffer_stack) )
+			YY_FATAL_ERROR( "out of dynamic memory in yyensure_buffer_stack()" );
+								  
 		memset((yy_buffer_stack), 0, num_to_alloc * sizeof(struct yy_buffer_state*));
 				
 		(yy_buffer_stack_max) = num_to_alloc;
@@ -2299,7 +2317,9 @@ void yyFlexLexer::yyensure_buffer_stack(void)
 		(yy_buffer_stack) = static_cast<struct yy_buffer_state**>(OSGScanParseSkel_realloc
 								((yy_buffer_stack),
 								num_to_alloc * sizeof(struct yy_buffer_state*)
-                                ));
+                                 ));
+		if ( ! (yy_buffer_stack) )
+			YY_FATAL_ERROR( "out of dynamic memory in yyensure_buffer_stack()" );
 
 		/* zero only the new slots.*/
 		memset((yy_buffer_stack) + (yy_buffer_stack_max), 0, grow_size * sizeof(struct yy_buffer_state*));
@@ -2323,8 +2343,7 @@ void yyFlexLexer::yyensure_buffer_stack(void)
 			(yy_start_stack) = static_cast<int *>(OSGScanParseSkel_realloc(static_cast<void *>((yy_start_stack)),new_size  ));
 
 		if ( ! (yy_start_stack) )
-			YY_FATAL_ERROR(
-			"out of memory expanding start-condition stack" );
+			YY_FATAL_ERROR( "out of memory expanding start-condition stack" );
 		}
 
 	(yy_start_stack)[(yy_start_stack_ptr)++] = YY_START;
