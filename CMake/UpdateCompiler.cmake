8a8384cab735936e62e6370c91efac25d59f61c0
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2008-12-22 07:54:36 +0000

    fixed: missed from last commit (debugrt target)
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1644 4683daeb-ad0f-0410-a623-93161e962ae5

--- CMake/UpdateCompiler.cmake
+++ CMake/UpdateCompiler.cmake
@@ -51,3 +51,71 @@ IF(CMAKE_COMPILER_IS_GNUCC)
     ENDIF(LINUX)
 
 ENDIF(CMAKE_COMPILER_IS_GNUCC)
+
+IF(MSVC)
+
+    IF(CMAKE_BUILD_TYPE STREQUAL "DebugRT")
+
+        MESSAGE(STATUS "Apply DebugRT settings")
+
+        STRING(REPLACE " msvcprt.lib msvcrt.lib" "" 
+               TMP_CXX_STANDARD_LIBRARIES ${CMAKE_CXX_STANDARD_LIBRARIES})
+
+        STRING(REPLACE " msvcrt.lib" "" 
+               TMP_C_STANDARD_LIBRARIES ${CMAKE_C_STANDARD_LIBRARIES})
+
+        STRING(REGEX MATCH "msvcprtd.lib msvcrtd.lib" 
+                           MSVC_CXX_RES ${TMP_CXX_STANDARD_LIBRARIES})
+
+        STRING(REGEX MATCH " msvcrtd.lib" 
+                           MSVC_C_RES ${TMP_C_STANDARD_LIBRARIES})
+
+        IF(NOT MSVC_CXX_RES)
+            SET(TMP_CXX_STANDARD_LIBRARIES "${TMP_CXX_STANDARD_LIBRARIES} msvcprtd.lib msvcrtd.lib")
+
+
+            SET(CMAKE_CXX_STANDARD_LIBRARIES ${TMP_CXX_STANDARD_LIBRARIES} 
+                                             CACHE STRING "OpenSG defaults" FORCE )
+        ENDIF(NOT MSVC_CXX_RES)
+
+        IF(NOT MSVC_C_RES)
+            SET(TMP_C_STANDARD_LIBRARIES "${TMP_C_STANDARD_LIBRARIES} msvcrtd.lib")
+
+            SET(CMAKE_C_STANDARD_LIBRARIES ${TMP_C_STANDARD_LIBRARIES} 
+                                           CACHE STRING "OpenSG defaults" FORCE )
+        ENDIF(NOT MSVC_C_RES)
+
+    ELSE(CMAKE_BUILD_TYPE STREQUAL "DebugRT")
+
+        MESSAGE(STATUS "Apply Debug/Release settings")
+
+        STRING(REPLACE " msvcprtd.lib msvcrtd.lib" "" 
+               TMP_CXX_STANDARD_LIBRARIES ${CMAKE_CXX_STANDARD_LIBRARIES})
+
+        STRING(REPLACE " msvcrtd.lib" "" 
+               TMP_C_STANDARD_LIBRARIES ${CMAKE_C_STANDARD_LIBRARIES})
+
+        STRING(REGEX MATCH "msvcprt.lib msvcrt.lib" 
+                           MSVC_CXX_RES ${TMP_CXX_STANDARD_LIBRARIES})
+
+        STRING(REGEX MATCH "msvcrt.lib" 
+                           MSVC_C_RES ${TMP_C_STANDARD_LIBRARIES})
+
+        IF(NOT MSVC_CXX_RES)
+            SET(TMP_CXX_STANDARD_LIBRARIES "${TMP_CXX_STANDARD_LIBRARIES} msvcprt.lib msvcrt.lib")
+
+
+            SET(CMAKE_CXX_STANDARD_LIBRARIES ${TMP_CXX_STANDARD_LIBRARIES} 
+                                             CACHE STRING "OpenSG defaults" FORCE )
+        ENDIF(NOT MSVC_CXX_RES)
+
+        IF(NOT MSVC_C_RES)
+            SET(TMP_C_STANDARD_LIBRARIES "${TMP_C_STANDARD_LIBRARIES} msvcrt.lib")
+
+            SET(CMAKE_C_STANDARD_LIBRARIES ${TMP_C_STANDARD_LIBRARIES} 
+                                           CACHE STRING "OpenSG defaults" FORCE )
+        ENDIF(NOT MSVC_C_RES)
+
+    ENDIF(CMAKE_BUILD_TYPE STREQUAL "DebugRT")
+
+ENDIF(MSVC)
