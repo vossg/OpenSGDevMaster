1f3779ea0faf7f480e40c1d95035903cfb848eeb
cneumann <cneumann@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-01-05 21:20:07 +0000

    changed: use one CMakeList.Lib.<name>.txt file per lib        : use explicit, manual listing of directories that belong to a lib        : move build functions to separate file
    Note: Libraries only contain sources in directories listed in the "per lib file",
          directories not listed are not scanned for sources!
          Adding a new source directory requires adding it to the lib file it belongs to.
    
    
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1647 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ CMake/BuildFunctions.cmake
@@ -0,0 +1,402 @@
+
+# register project with build
+MACRO(OSG_ADD_PROJECT)
+    IF(${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+        OPTION(OSGBUILD_${PROJECT_NAME} "Build the ${PROJECT_NAME} library" ON)
+    ENDIF(${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+ENDMACRO(OSG_ADD_PROJECT)
+
+
+# select the current project as the one settings are added to
+MACRO(OSG_SELECT_PROJECT)
+
+    IF(OSGBUILD_${PROJECT_NAME})
+        MESSAGE(STATUS "Processing ${PROJECT_NAME}")
+    ELSE(OSGBUILD_${PROJECT_NAME})
+        RETURN()
+    ENDIF(OSGBUILD_${PROJECT_NAME})
+
+    SET(${PROJECT_NAME}_CONFIG_FILE
+        "${CMAKE_BINARY_DIR}/${PROJECT_NAME}.cmake")
+
+    IF(${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+        FILE(WRITE ${${PROJECT_NAME}_CONFIG_FILE}
+            "# ${PROJECT_NAME}.cmake -- auto generated\n\n")
+
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_SRC)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_HDR)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_INL)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_INS)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_FCD)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_LL)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_YY)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_UNITTEST_SRC)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_TEST_SRC)\n")
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "SET(${PROJECT_NAME}_INC)\n\n")
+    ENDIF(${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+
+    # clear dependency variables
+    SET(${PROJECT_NAME}_DEP_OSG_LIB)
+    SET(${PROJECT_NAME}_DEP_TEST_OSG_LIB)
+
+    SET(${PROJECT_NAME}_DEP_LIB)
+    SET(${PROJECT_NAME}_DEP_LIBDIR)
+    SET(${PROJECT_NAME}_DEP_INCDIR)
+
+    SET(${PROJECT_NAME}_DEP_TEST_LIB)
+    SET(${PROJECT_NAME}_DEP_TEST_LIBDIR)
+    SET(${PROJECT_NAME}_DEP_TEST_INCDIR)
+
+    SET(${PROJECT_NAME}_DEP_ADD_INCDIR)
+ENDMACRO(OSG_SELECT_PROJECT)
+
+
+# write the dependecy variables to the ${${PROJECT_NAME}_CONFIG_FILE file
+FUNCTION(OSG_STORE_PROJECT_DEPENDENCIES)
+    IF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+        RETURN()
+    ENDIF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+
+    # dependencies - OpenSG
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_OSG_LIB ${${PROJECT_NAME}_DEP_OSG_LIB})\n\n")
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_TEST_OSG_LIB ${${PROJECT_NAME}_DEP_TEST_OSG_LIB})\n\n")
+
+    # dependencies - external
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_LIB ${${PROJECT_NAME}_DEP_LIB})\n\n")
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_LIBDIR ${${PROJECT_NAME}_DEP_LIBDIR})\n\n")
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_INCDIR ${${PROJECT_NAME}_DEP_INCDIR})\n\n")
+
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_TEST_LIB ${${PROJECT_NAME}_TEST_DEP_LIB})\n\n")
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_TEST_LIBDIR ${${PROJECT_NAME}_TEST_DEP_LIBDIR})\n\n")
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_TEST_INCDIR ${${PROJECT_NAME}_TEST_DEP_INCDIR})\n\n")
+
+    # dependencies - additional
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+        "SET(${PROJECT_NAME}_DEP_ADD_INCDIR ${${PROJECT_NAME}_DEP_ADD_INCDIR})\n\n")
+
+ENDFUNCTION(OSG_STORE_PROJECT_DEPENDENCIES)
+
+
+# add directory DIRNAME to current project
+FUNCTION(OSG_ADD_DIRECTORY DIRNAME)
+
+    IF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+        RETURN()
+    ENDIF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGCOLLECT")
+
+    MESSAGE(STATUS "Adding directory: ${DIRNAME}")
+
+    FILE(GLOB LOCAL_SRC          "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.cpp")
+    FILE(GLOB LOCAL_HDR          "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.h")
+    FILE(GLOB LOCAL_INL          "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.inl")
+    FILE(GLOB LOCAL_INS          "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.ins")
+    FILE(GLOB LOCAL_FCD          "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.fcd")
+    FILE(GLOB LOCAL_LL           "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.ll")
+    FILE(GLOB LOCAL_YY           "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*.yy")
+    FILE(GLOB LOCAL_UNITTEST_SRC "${CMAKE_SOURCE_DIR}/${DIRNAME}/OSG*Test.cpp")
+    FILE(GLOB LOCAL_TEST_SRC     "${CMAKE_SOURCE_DIR}/${DIRNAME}/test*.cpp")
+
+    # filter unittest sources
+    IF(LOCAL_UNITTEST_SRC)
+        LIST(REMOVE_ITEM LOCAL_SRC ${LOCAL_UNITTEST_SRC})
+    ENDIF(LOCAL_UNITTEST_SRC)
+
+    # store file lists
+
+    # library sources
+    IF(LOCAL_SRC)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_SRC \"${LOCAL_SRC}\")\n\n")
+    ENDIF(LOCAL_SRC)
+
+    IF(LOCAL_HDR)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_HDR \"${LOCAL_HDR}\")\n\n")
+    ENDIF(LOCAL_HDR)
+
+    IF(LOCAL_INL)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_INL \"${LOCAL_INL}\")\n\n")
+    ENDIF(LOCAL_INL)
+
+    IF(LOCAL_INS)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_INS \"${LOCAL_INS}\")\n\n")
+    ENDIF(LOCAL_INS)
+
+    IF(LOCAL_FCD)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_FCD \"${LOCAL_FCD}\")\n\n")
+    ENDIF(LOCAL_FCD)
+
+    IF(LOCAL_LL)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_LL \"${LOCAL_LL}\")\n\n")
+    ENDIF(LOCAL_LL)
+
+    IF(LOCAL_YY)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_YY \"${LOCAL_YY}\")\n\n")
+    ENDIF(LOCAL_YY)
+
+    # unittests
+    IF(LOCAL_UNITTEST_SRC)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+            "LIST(APPEND ${PROJECT_NAME}_UNITTEST_SRC \"${LOCAL_UNITTEST_SRC}\")\n\n")
+    ENDIF(LOCAL_UNITTEST_SRC)
+
+    # tests
+    IF(LOCAL_TEST_SRC)
+        FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+             "LIST(APPEND ${PROJECT_NAME}_TEST_SRC \"${LOCAL_TEST_SRC}\")\n\n")
+    ENDIF(LOCAL_TEST_SRC)
+
+    FILE(APPEND ${${PROJECT_NAME}_CONFIG_FILE}
+         "LIST(APPEND ${PROJECT_NAME}_INC \"${CMAKE_SOURCE_DIR}/${DIRNAME}\")\n\n")
+
+ENDFUNCTION(OSG_ADD_DIRECTORY)
+
+# perform default actions for pass OSGSETUP
+FUNCTION(OSG_SETUP_PROJECT PROJ_DEFINE)
+
+    IF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGSETUP")
+        RETURN()
+    ENDIF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGSETUP")
+
+    # read file lists
+    INCLUDE(${${PROJECT_NAME}_CONFIG_FILE})
+
+    #####
+    # FCD
+    #####
+
+    IF(OSG_ENABLE_FCD2CODE AND PYTHONINTERP_FOUND)
+
+        FOREACH(FCDFile ${${PROJECT_NAME}_FCD})
+            GET_FILENAME_COMPONENT(FCDBase ${FCDFile} NAME_WE)
+            GET_FILENAME_COMPONENT(FCDDir  ${FCDFile} PATH)
+
+            SET(FCDBaseHdr "${FCDBase}Base.h")
+            SET(FCDBaseCpp "${FCDBase}Base.cpp")
+            SET(FCDBaseInl "${FCDBase}Base.inl")
+            SET(FCDBaseFld "${FCDBase}Fields.h")
+
+            IF(NOT EXISTS ${FCDDir}/${FCDBaseCpp})
+                SET(${PROJECT_NAME}_SRC ${${PROJECT_NAME}_SRC} ${FCDDir}/${FCDBaseCpp})
+                SET(${PROJECT_NAME}_HDR ${${PROJECT_NAME}_HDR} ${FCDDir}/${FCDBaseHds})
+                SET(${PROJECT_NAME}_INL ${${PROJECT_NAME}_INL} ${FCDDir}/${FCDBaseInl})
+                SET(${PROJECT_NAME}_HDR ${${PROJECT_NAME}_HDR} ${FCDDir}/${FCDBaseFld})
+            ENDIF(NOT EXISTS ${FCDDir}/${FCDBaseCpp})
+
+            SET(FCDCommand ${CMAKE_SOURCE_DIR}/Tools/fcd2code/fcd2code)
+
+            ADD_CUSTOM_COMMAND(
+                OUTPUT ${FCDDir}/${FCDBaseHdr}
+                        ${FCDDir}/${FCDBaseCpp}
+                        ${FCDDir}/${FCDBaseInl}
+                        ${FCDDir}/${FCDBaseFld}
+                COMMAND ${PYTHON_EXECUTABLE} ${FCDCommand} -c -b -d ${FCDFile} -p ${FCDDir} -r ${CMAKE_SOURCE_DIR}
+                MAIN_DEPENDENCY ${FCDFile})
+        ENDFOREACH(FCDFile)
+
+    ENDIF(OSG_ENABLE_FCD2CODE AND PYTHONINTERP_FOUND)
+
+    ############
+    # Flex/Bison
+    ############
+
+    IF(OSG_ENABLE_SCANPARSE_REGEN AND FLEX_EXE AND BISON_EXE)
+
+        FOREACH(YYFile ${${PROJECT_NAME}_YY})
+
+            GET_FILENAME_COMPONENT(YYBase ${YYFile} NAME_WE)
+
+            STRING(REPLACE "Parser" "_" YYOpt ${YYBase})
+
+            SET(YYSrc ${CMAKE_CURRENT_BINARY_DIR}/${YYBase}.cpp)
+            SET(YYHdr ${CMAKE_CURRENT_BINARY_DIR}/${YYBase}.hpp)
+
+            ADD_CUSTOM_COMMAND(
+                OUTPUT ${YYSrc} ${YYHdr}
+                COMMAND ${BISON_EXE} -d -v -p${YYOpt} -b${YYOpt} -o ${YYSrc} ${YYFile}
+                MAIN_DEPENDENCY ${YYFile})
+
+            SET(${PROJECT_NAME}_SRC ${${PROJECT_NAME}_SRC} ${YYSrc})
+            SET(${PROJECT_NAME}_HDR ${${PROJECT_NAME}_HDR} ${YYHdr})
+
+        ENDFOREACH()
+
+        FOREACH(LLFile ${${PROJECT_NAME}_LL})
+
+            GET_FILENAME_COMPONENT(LLBase ${LLFile} NAME_WE)
+
+            STRING(REPLACE "Scanner" "_" LLOpt ${LLBase})
+
+            SET(LLSrc ${CMAKE_CURRENT_BINARY_DIR}/${LLBase}.cpp)
+
+            ADD_CUSTOM_COMMAND(
+                OUTPUT ${LLSrc}
+                COMMAND ${FLEX_EXE} -+ -P${LLOpt} -t ${LLFile} >  ${LLSrc}
+                MAIN_DEPENDENCY ${LLFile})
+
+            SET(${PROJECT_NAME}_SRC ${${PROJECT_NAME}_SRC} ${LLSrc})
+
+        ENDFOREACH()
+
+        IF(${PROJECT_NAME}_YY OR ${PROJECT_NAME}_LL)
+            SET(${PROJECT_NAME}_INC ${${PROJECT_NAME}_INC} ${CMAKE_CURRENT_BINARY_DIR})
+        ENDIF(${PROJECT_NAME}_YY OR ${PROJECT_NAME}_LL)
+
+    ENDIF(OSG_ENABLE_SCANPARSE_REGEN AND FLEX_EXE AND BISON_EXE)
+
+    ###############
+    # build the lib
+    ###############
+
+    ADD_LIBRARY(${PROJECT_NAME} ${${PROJECT_NAME}_SRC})
+
+    INCLUDE_DIRECTORIES(${${PROJECT_NAME}_INC})
+
+    SET_TARGET_PROPERTIES(${PROJECT_NAME} PROPERTIES
+        DEFINE_SYMBOL ${PROJ_DEFINE}
+        VERSION 2.0.0
+        SOVERSION 2.0.0
+        DEBUGRT_POSTFIX "_d")
+
+    # dependencies - OpenSG
+    FOREACH(OSGDEP ${${PROJECT_NAME}_DEP_OSG_LIB})
+        IF(NOT EXISTS "${CMAKE_BINARY_DIR}/${OSGDEP}.cmake")
+            MESSAGE(FATAL_ERROR "Dependency (${OSGDEP}) not found, "
+                                "can not build ${PROJECT_NAME}\n")
+            RETURN()
+        ENDIF(NOT EXISTS "${CMAKE_BINARY_DIR}/${OSGDEP}.cmake")
+
+        INCLUDE("${CMAKE_BINARY_DIR}/${OSGDEP}.cmake")
+        INCLUDE_DIRECTORIES(${${OSGDEP}_INC})
+
+        ADD_DEPENDENCIES(${PROJECT_NAME} ${OSGDEP})
+
+        TARGET_LINK_LIBRARIES(${PROJECT_NAME} ${OSGDEP})
+    ENDFOREACH(OSGDEP)
+
+    # dependencies - External
+    FOREACH(INCDIR ${${PROJECT_NAME}_DEP_INCDIR})
+        INCLUDE_DIRECTORIES(${${INCDIR}})
+    ENDFOREACH(INCDIR)
+
+    FOREACH(LIBDIR ${${PROJECT_NAME}_DEP_LIBDIR})
+        LINK_DIRECTORIES(${LIBDIR})
+    ENDFOREACH(LIBDIR)
+
+    FOREACH(LIB ${${PROJECT_NAME}_DEP_LIB})
+        TARGET_LINK_LIBRARIES(${PROJECT_NAME} ${${LIB}})
+    ENDFOREACH(LIB)
+
+    # install rules
+    INSTALL(TARGETS ${PROJECT_NAME}
+            RUNTIME DESTINATION lib${OSG_LIBDIR_SUFFIX}
+            LIBRARY DESTINATION lib${OSG_LIBDIR_SUFFIX}
+            ARCHIVE DESTINATION lib${OSG_LIBDIR_SUFFIX})
+
+    INSTALL(FILES ${${PROJECT_NAME}_HDR}
+            DESTINATION include/OpenSG
+            PERMISSIONS OWNER_WRITE OWNER_READ
+                        GROUP_READ
+                        WORLD_READ)
+
+    INSTALL(FILES ${${PROJECT_NAME}_INL}
+            DESTINATION include/OpenSG
+            PERMISSIONS OWNER_WRITE OWNER_READ
+                        GROUP_READ
+                        WORLD_READ)
+
+    INSTALL(FILES ${${PROJECT_NAME}_INS}
+            DESTINATION include/OpenSG
+            PERMISSIONS OWNER_WRITE OWNER_READ
+                        GROUP_READ
+                        WORLD_READ)
+
+ENDFUNCTION(OSG_SETUP_PROJECT PROJ_DEFINE)
+
+
+FUNCTION(OSG_SETUP_TEST_PROJECT)
+
+    IF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGSETUPTEST")
+        RETURN()
+    ENDIF(NOT ${OSG_CMAKE_PASS} STREQUAL "OSGSETUPTEST")
+
+    INCLUDE(${${PROJECT_NAME}_CONFIG_FILE})
+    INCLUDE_DIRECTORIES(${${PROJECT_NAME}_INC})
+
+    FOREACH(OSGDEP ${${PROJECT_NAME}_DEP_OSG_LIB})
+        IF(NOT EXISTS "${CMAKE_BINARY_DIR}/${OSGDEP}.cmake")
+            MESSAGE(FATAL_ERROR "Dependency (${OSGDEP}) not found, "
+                                "can not build tests for ${PROJECT_NAME}\n")
+            RETURN()
+        ENDIF(NOT EXISTS "${CMAKE_BINARY_DIR}/${OSGDEP}.cmake")
+
+        INCLUDE("${CMAKE_BINARY_DIR}/${OSGDEP}.cmake")
+        INCLUDE_DIRECTORIES(${${OSGDEP}_INC})
+    ENDFOREACH(OSGDEP)
+
+    # dependencies - External
+    FOREACH(INCDIR ${${PROJECT_NAME}_DEP_INCDIR})
+        INCLUDE_DIRECTORIES(${${INCDIR}})
+    ENDFOREACH(INCDIR)
+
+    FOREACH(LIBDIR ${${PROJECT_NAME}_DEP_LIBDIR})
+        LINK_DIRECTORIES(${${LIBDIR}})
+    ENDFOREACH(LIBDIR)
+
+    FOREACH(OSGTESTDEP ${${PROJECT_NAME}_DEP_TEST_OSG_LIB})
+        IF(NOT EXISTS "${CMAKE_BINARY_DIR}/${OSGTESTDEP}.cmake")
+            MESSAGE(STATUS "Test dependency (${OSGTESTDEP}) not found, "
+                           "ignoring tests for ${PROJECT_NAME}\n")
+
+            RETURN()
+        ENDIF(NOT EXISTS "${CMAKE_BINARY_DIR}/${OSGTESTDEP}.cmake")
+
+        INCLUDE("${CMAKE_BINARY_DIR}/${OSGTESTDEP}.cmake")
+        INCLUDE_DIRECTORIES(${${OSGTESTDEP}_INC})
+    ENDFOREACH(OSGTESTDEP)
+
+    FOREACH(INCDIR ${${PROJECT_NAME}_DEP_TEST_INCDIR})
+        INCLUDE_DIRECTORIES(${${INCDIR}})
+    ENDFOREACH(INCDIR)
+
+    FOREACH(LIBDIR ${${PROJECT_NAME}_DEP_TEST_LIBDIR})
+        LINK_DIRECTORIES(${${LIBDIR}})
+    ENDFOREACH(LIBDIR)
+
+    FOREACH(EXE_SRC ${${PROJECT_NAME}_TEST_SRC})
+        GET_FILENAME_COMPONENT(EXE ${EXE_SRC} NAME_WE)
+
+        ADD_EXECUTABLE(${EXE} ${EXE_SRC})
+
+        FOREACH(OSGTESTDEP ${${PROJECT_NAME}_DEP_TEST_OSG_LIB})
+            ADD_DEPENDENCIES(${EXE} ${OSGTESTDEP})
+            TARGET_LINK_LIBRARIES(${EXE} ${OSGTESTDEP})
+        ENDFOREACH(OSGTESTDEP)
+
+        TARGET_LINK_LIBRARIES(${EXE} ${PROJECT_NAME})
+    ENDFOREACH(EXE_SRC)
+
+ENDFUNCTION(OSG_SETUP_TEST_PROJECT)
--- CMake/UpdateCompiler.cmake
+++ CMake/UpdateCompiler.cmake
@@ -8,11 +8,15 @@ IF(CMAKE_COMPILER_IS_GNUCC)
 
             MESSAGE(STATUS "Apply sse2 settings")
 
-            string(REGEX MATCH "-msse2 -mfpmath=sse" 
-                               SSE2_CXX_RES ${CMAKE_CXX_FLAGS})
+            IF(CMAKE_CXX_FLAGS)
+                string(REGEX MATCH "-msse2 -mfpmath=sse" 
+                                   SSE2_CXX_RES ${CMAKE_CXX_FLAGS})
+            ENDIF(CMAKE_CXX_FLAGS)
 
-            string(REGEX MATCH "-msse2 -mfpmath=sse" 
-                               SSE2_C_RES ${CMAKE_C_FLAGS})
+            IF(CMAKE_C_FLAGS)
+                string(REGEX MATCH "-msse2 -mfpmath=sse" 
+                                   SSE2_C_RES ${CMAKE_C_FLAGS})
+            ENDIF(CMAKE_C_FLAGS)
 
             IF(NOT SSE2_CXX_RES)
                 SET(TMP_CXX_FLAGS "${CMAKE_CXX_FLAGS} -msse2 -mfpmath=sse")
@@ -35,11 +39,15 @@ IF(CMAKE_COMPILER_IS_GNUCC)
 
             MESSAGE(STATUS "Remove sse2 settings")
 
-            STRING(REPLACE " -msse2 -mfpmath=sse" "" 
-                   TMP_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+            IF(CMAKE_CXX_FLAGS)
+                STRING(REPLACE " -msse2 -mfpmath=sse" "" 
+                       TMP_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+            ENDIF(CMAKE_CXX_FLAGS)
 
-            STRING(REPLACE " -msse2 -mfpmath=sse" "" 
-                   TMP_C_FLAGS ${CMAKE_C_FLAGS})
+            IF(CMAKE_C_FLAGS)
+                STRING(REPLACE " -msse2 -mfpmath=sse" "" 
+                       TMP_C_FLAGS ${CMAKE_C_FLAGS})
+            ENDIF(CMAKE_C_FLAGS)
 
             SET(CMAKE_CXX_FLAGS ${TMP_CXX_FLAGS} 
                                 CACHE STRING "OpenSG defaults" FORCE )
