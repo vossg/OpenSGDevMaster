8a0ab740abb2e10f2312cc74eaf76fe6ca75b735
cneumann <cneumann@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-01-23 17:21:09 +0000

    added: LogicOpChunk
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1685 4683daeb-ad0f-0410-a623-93161e962ae5

---
+++ Source/System/State/OpenGL/OSGLogicOpChunk.cpp
@@ -0,0 +1,184 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2006 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+//---------------------------------------------------------------------------
+//  Includes
+//---------------------------------------------------------------------------
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <OSGConfig.h>
+
+#include "OSGLogicOpChunk.h"
+
+OSG_BEGIN_NAMESPACE
+
+// Documentation for this class is emitted in the
+// OSGLogicOpChunkBase.cpp file.
+// To modify it, please change the .fcd file (OSGLogicOpChunk.fcd) and
+// regenerate the base file.
+
+/***************************************************************************\
+ *                           Class variables                               *
+\***************************************************************************/
+
+StateChunkClass LogicOpChunk::_class("LogicOp");
+
+/***************************************************************************\
+ *                           Class methods                                 *
+\***************************************************************************/
+
+void LogicOpChunk::initMethod(InitPhase ePhase)
+{
+    Inherited::initMethod(ePhase);
+}
+
+
+/***************************************************************************\
+ *                           Instance methods                              *
+\***************************************************************************/
+
+/*------------------------- Chunk Class Access ---------------------------*/
+
+const StateChunkClass *LogicOpChunk::getClass(void) const
+{
+    return &_class;
+}
+
+/*--------------------------------- Sync ---------------------------------*/
+
+void LogicOpChunk::changed(ConstFieldMaskArg whichField, 
+                            UInt32            origin,
+                            BitVector         details)
+{
+    Inherited::changed(whichField, origin, details);
+}
+
+/*-------------------------------- Output --------------------------------*/
+
+void LogicOpChunk::dump(      UInt32    ,
+                         const BitVector ) const
+{
+    SLOG << "Dump LogicOpChunk NI" << std::endl;
+}
+
+/*----------------------------- State Commands ---------------------------*/
+
+void LogicOpChunk::activate(DrawEnv *drawEnv, UInt32 index)
+{
+    if(_sfLogicOp.getValue() != GL_COPY)
+    {
+        glLogicOp(_sfLogicOp.getValue());
+        glEnable (GL_COLOR_LOGIC_OP    );
+    }
+}
+
+void LogicOpChunk::changeFrom(DrawEnv    *drawEnv,
+                              StateChunk *old,
+                              UInt32      index  )
+{
+    old ->deactivate(drawEnv, index);
+    this->activate  (drawEnv, index);
+}
+
+void LogicOpChunk::deactivate(DrawEnv *drawEnv, UInt32 index)
+{
+    if(_sfLogicOp.getValue() != GL_COPY)
+    {
+        glDisable(GL_COLOR_LOGIC_OP);
+        glLogicOp(GL_COPY          );
+    }
+}
+
+/*------------------------------- Comparison -----------------------------*/
+
+Real32 LogicOpChunk::switchCost(StateChunk *chunk)
+{
+    return 0.f;
+}
+
+bool LogicOpChunk::operator < (const StateChunk &other) const
+{
+    return this < &other;
+}
+
+bool LogicOpChunk::operator == (const StateChunk &other) const
+{
+    LogicOpChunk const *tOther = dynamic_cast<LogicOpChunk const *>(&other);
+
+    if(tOther == NULL)
+        return false;
+
+    if(tOther == this)
+        return true;
+
+    if(getLogicOp() != tOther->getLogicOp())
+        return false;
+
+    return true;
+}
+
+bool LogicOpChunk::operator != (const StateChunk &other) const
+{
+    return !(*this == other);
+}
+
+/*-------------------------------------------------------------------------*\
+ -  private                                                                 -
+\*-------------------------------------------------------------------------*/
+
+/*----------------------- constructors & destructors ----------------------*/
+
+LogicOpChunk::LogicOpChunk(void) :
+    Inherited()
+{
+}
+
+LogicOpChunk::LogicOpChunk(const LogicOpChunk &source) :
+    Inherited(source)
+{
+}
+
+LogicOpChunk::~LogicOpChunk(void)
+{
+}
+
+/*----------------------------- class specific ----------------------------*/
+
+OSG_END_NAMESPACE
---
+++ Source/System/State/OpenGL/OSGLogicOpChunk.h
@@ -0,0 +1,167 @@
+/*---------------------------------------------------------------------------*\
+ *                                OpenSG                                     *
+ *                                                                           *
+ *                                                                           *
+ *               Copyright (C) 2000-2006 by the OpenSG Forum                 *
+ *                                                                           *
+ *                            www.opensg.org                                 *
+ *                                                                           *
+ *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                License                                    *
+ *                                                                           *
+ * This library is free software; you can redistribute it and/or modify it   *
+ * under the terms of the GNU Library General Public License as published    *
+ * by the Free Software Foundation, version 2.                               *
+ *                                                                           *
+ * This library is distributed in the hope that it will be useful, but       *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of                *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU         *
+ * Library General Public License for more details.                          *
+ *                                                                           *
+ * You should have received a copy of the GNU Library General Public         *
+ * License along with this library; if not, write to the Free Software       *
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*\
+ *                                Changes                                    *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+ *                                                                           *
+\*---------------------------------------------------------------------------*/
+
+#ifndef _OSGLOGICOPCHUNK_H_
+#define _OSGLOGICOPCHUNK_H_
+#ifdef __sgi
+#pragma once
+#endif
+
+#include "OSGLogicOpChunkBase.h"
+
+OSG_BEGIN_NAMESPACE
+
+/*! \brief LogicOpChunk class. See \ref
+           PageStateLogicOpChunk for a description.
+*/
+
+class OSG_STATE_DLLMAPPING LogicOpChunk : public LogicOpChunkBase
+{
+    /*==========================  PUBLIC  =================================*/
+  public:
+
+    typedef LogicOpChunkBase Inherited;
+    typedef LogicOpChunk     Self;
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                 Chunk Class Access                           */
+    /*! \{                                                                 */
+
+    virtual const StateChunkClass *getClass(void) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name              Static Chunk Class Access                       */
+    /*! \{                                                                 */
+
+    static       UInt32            getStaticClassId(void);
+    static const StateChunkClass * getStaticClass  (void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Sync                                    */
+    /*! \{                                                                 */
+
+    virtual void changed(ConstFieldMaskArg whichField,
+                         UInt32            origin,
+                         BitVector         details    );
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                     Output                                   */
+    /*! \{                                                                 */
+
+    virtual void dump(      UInt32     uiIndent = 0,
+                      const BitVector  bvFlags  = 0) const;
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                    State Commands                            */
+    /*! \{                                                                 */
+
+    virtual void activate  (DrawEnv    *action,
+                            UInt32      index = 0);
+
+    virtual void changeFrom(DrawEnv    *action,
+                            StateChunk *old,
+                            UInt32      index = 0);
+
+    virtual void deactivate(DrawEnv    *action,
+                            UInt32      index = 0);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Comparison                                 */
+    /*! \{                                                                 */
+
+    virtual Real32 switchCost(StateChunk *chunk);
+
+    virtual bool   operator <  (const StateChunk &other) const;
+
+    virtual bool   operator == (const StateChunk &other) const;
+    virtual bool   operator != (const StateChunk &other) const;
+
+    /*=========================  PROTECTED  ===============================*/
+  protected:
+
+    // Variables should all be in LogicOpChunkBase.
+
+    /*---------------------------------------------------------------------*/
+    /*! \name                  Constructors                                */
+    /*! \{                                                                 */
+
+    LogicOpChunk(void);
+    LogicOpChunk(const LogicOpChunk &source);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                   Destructors                                */
+    /*! \{                                                                 */
+
+    virtual ~LogicOpChunk(void);
+
+    /*! \}                                                                 */
+    /*---------------------------------------------------------------------*/
+    /*! \name                      Init                                    */
+    /*! \{                                                                 */
+
+    static void initMethod(InitPhase ePhase);
+
+    /*! \}                                                                 */
+    /*==========================  PRIVATE  ================================*/
+
+  private:
+
+    friend class FieldContainer;
+    friend class LogicOpChunkBase;
+
+    // class. Used for indexing in State
+    static StateChunkClass _class;
+
+    // prohibit default functions (move to 'public' if you need one)
+    void operator =(const LogicOpChunk &source);
+};
+
+typedef LogicOpChunk *LogicOpChunkP;
+
+OSG_END_NAMESPACE
+
+#include "OSGLogicOpChunkBase.inl"
+#include "OSGLogicOpChunk.inl"
+
+#endif /* _OSGLOGICOPCHUNK_H_ */
