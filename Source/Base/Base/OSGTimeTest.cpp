1c4874bcfcf03fccc28a3e319fa4174df249c863
patrick <patrick@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-02-19 23:56:42 +0000

    Merged the rest of r1475 from the fcptr_stable_jun07 branch:
       Add more tests for time functions.
    
    
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1759 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/Base/Base/OSGTimeTest.cpp
+++ Source/Base/Base/OSGTimeTest.cpp
@@ -2,13 +2,16 @@
  *                                OpenSG                                     *
  *                                                                           *
  *                                                                           *
- *               Copyright (C) 2000-2006 by the OpenSG Forum                 *
+ *             Copyright (C) 2000-2006 by the OpenSG Forum                   *
+ *                                                                           *
+ *                            www.opensg.org                                 *
  *                                                                           *
  *   contact: dirk@opensg.org, gerrit.voss@vossg.org, jbehr@zgdv.de          *
  *                                                                           *
 \*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*\
  *                                License                                    *
+ *                                                                           *
  *                                                                           *
  * This library is free software; you can redistribute it and/or modify it   *
  * under the terms of the GNU Library General Public License as published    *
@@ -23,6 +26,7 @@
  * License along with this library; if not, write to the Free Software       *
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.                 *
  *                                                                           *
+ *                                                                           *
 \*---------------------------------------------------------------------------*/
 /*---------------------------------------------------------------------------*\
  *                                Changes                                    *
@@ -34,31 +38,71 @@
  *                                                                           *
 \*---------------------------------------------------------------------------*/
 
-#include <UnitTest++.h>
+#include "UnitTest++.h"
 
-// Unit tests for time classes
-
-#include <OpenSG/OSGTime.h>
+#include "OpenSG/OSGConfig.h"
+#include "OpenSG/OSGTime.h"
 #include <iostream>
 
-OSG_USING_NAMESPACE
+using namespace UnitTest;
 
 SUITE(TimeTests)
 {
 
-TEST(TimeShouldMoveForward)
+TEST(TestCallingAll)
 {
-    // On some OSs/systems (especially multi-core) time can move backwards
-    // when it does, all sorts of code can break in very strange ways
-    OSG::TimeStamp prev_time = OSG::getTimeStamp();
+   CHECK( OSG::getSystemTime() > 0.0);
+   CHECK( OSG::getTimeStamp() > 0);
+   OSG::Real64 ts_ms(OSG::getTimeStampMsecs(OSG::getTimeStamp()) );
 
-    for(unsigned i=0;i<1000;i++)
-    {
-    	OSG::TimeStamp cur_time = OSG::getTimeStamp();
-        CHECK(cur_time >= prev_time);
-        prev_time = cur_time;
-        //std::cout << "time: " << cur_time << std::endl;
-    }
+   if (! (ts_ms>0.0))
+   {  std::cout << "ts_ms: " << ts_ms << std::endl; }
+   
+   CHECK( ts_ms > 0.0);
+}
+
+TEST(CheckMSecs)
+{
+   //std::cout << "freq: " << OSG::getTimeStampFreq() << std::endl;
+   //std::cout << std::setprecision(16) << std::fixed;
+   //std::cout << "Time: " << OSG::getSystemTime() << std::endl;
+   //std::cout << "TS: " << OSG::getTimeStamp() << std::endl;
+   //std::cout << "Msecs: " << OSG::getTimeStampMsecs(OSG::getTimeStamp()) << std::endl;
+
+   OSG::Real64 start_ms = OSG::getTimeStampMsecs(OSG::getTimeStamp());
+   OSG::osgSleep(200);
+   OSG::Real64 end_ms   = OSG::getTimeStampMsecs(OSG::getTimeStamp());
+   OSG::Real64 ms_diff  = end_ms-start_ms;
+
+   // Check that the time has changed a bit but not too much
+   CHECK( (ms_diff > 100) && (ms_diff < 400));
+
+   //std::cout << "Time: " << OSG::getSystemTime() << std::endl;
+   //std::cout << "TS: " << OSG::getTimeStamp() << std::endl;
+   //std::cout << "Msecs: " << OSG::getTimeStampMsecs(OSG::getTimeStamp()) << std::endl;
+   //std::cout << "ms_diff: " << ms_diff << std::endl;
+}
+
+TEST(TimeStampMovesForward)
+{
+   // With some version of Linux, the assembly version of the timestamp code
+   // used to have issues with going backward during context switches and the like
+   OSG::UInt64 prev_ts(0);
+
+   for (unsigned i=0; i<100; i++)
+   {
+      OSG::UInt64 cur_ts = OSG::getTimeStamp();
+      //std::cout << " cur: " << cur_ts  << "  diff: " << (cur_ts-prev_ts) << std::endl;
+      if (cur_ts < prev_ts)
+      {
+        std::cout << " cur: " << cur_ts  << "  diff: " << (cur_ts-prev_ts) << std::endl;
+      }
+
+      CHECK(cur_ts >= prev_ts);
+      OSG::osgSleep(0);  // yield
+      prev_ts = cur_ts;
+   }
+
 }
 
 } // SUITE
