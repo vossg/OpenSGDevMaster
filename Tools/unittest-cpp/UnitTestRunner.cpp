cb71e454e96f6656e6587233442db42f5a1d219c
cneumann <cneumann@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-04-01 15:32:12 +0000

    removed: svn:externals for unittest-cpp on Tools/ it moved to Tools/unittest-cpp
    git-svn-id: https://opensg.vrsource.org/svn/trunk@1854 4683daeb-ad0f-0410-a623-93161e962ae5

--- Tools/unittest-cpp/UnitTestRunner.cpp
+++ Tools/unittest-cpp/UnitTestRunner.cpp
@@ -5,12 +5,42 @@
 #include <OSGConfig.h>
 #include <OSGBaseInitFunctions.h>
 
+struct TestNameEquals
+{
+	TestNameEquals(const std::string &testName)
+		: _testName(testName)
+	{
+	}
+
+	bool operator()(const UnitTest::Test * const test) const
+	{
+		return (_testName == test->m_details.testName);
+	}
+
+	std::string _testName;
+};
+
 int main(int argc, char *argv[])
 {
     OSG::osgInit(argc, argv);
 
-    return UnitTest::RunAllTests();
+	int retVal = -1;
 
-//     UnitTest::TestReporterStdout rep;
-//     return UnitTest::RunAllTests(rep, UnitTest::Test::GetTestList(), argv[1]);
+	if(argc > 1)
+	{
+		TestNameEquals               pred(argv[1]);
+		UnitTest::TestReporterStdout rep;
+		UnitTest::TestRunner         runner(rep);
+
+		retVal = runner.RunTestsIf(
+			UnitTest::Test::GetTestList(), NULL, pred, 0);
+	}
+	else
+	{
+		retVal = UnitTest::RunAllTests();
+	}
+
+	OSG::osgExit();
+
+    return retVal;
 }
