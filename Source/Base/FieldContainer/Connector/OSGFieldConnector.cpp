vossg/OpenSGDevMaster

commit 99e0c1fec59081b0cf06f81f57ecac505f4e389c

fixed: field connector: subConnection(foo, "f1", NULL, NULL) case
git-svn-id: https://opensg.vrsource.org/svn/trunk@2019 4683daeb-ad0f-0410-a623-93161e962ae5

Source/Base/FieldContainer/Connector/OSGFieldConnector.cpp
@@ -112,9 +112,11 @@ bool addConnection(      OSG::AttachmentContainer *pSrcContainer,
 
     if(pSrcDesc == NULL || pDstDesc == NULL)
     {
-        FWARNING(("Failed desc %p %p | %s %s\n", 
-                  pSrcDesc, pDstDesc,
-                  szSrcName, szDstName));
+        FWARNING(("addConnection: Failed to obtain field descriptions for "
+                  "source container [%p] field [%s] desc [%p] - "
+                  "destination container [%p] field [%s] desc [%p]\n",
+                  pSrcContainer, szSrcName, pSrcDesc,
+                  pDstContainer, szDstName, pDstDesc));
 
         return false;
     }
@@ -130,9 +132,9 @@ bool addConnection(      OSG::AttachmentContainer *pSrcContainer,
           
     if(pSrcContainer == NULL || pDstContainer == NULL)
     {
-        FWARNING(("Failed handle container %p %p\n", 
-                  pSrcContainer, 
-                  pDstContainer));
+        FWARNING(("addConnection: Failed to obtain field handles for "
+                  "source container [%p] - destination container [%p]\n",
+                  pSrcContainer, pDstContainer));
 
         return false;
     }
@@ -227,17 +229,11 @@ bool subConnection(      OSG::AttachmentContainer *pSrcContainer,
         }
     }
 
-    pSrcContainer = 
-              dynamic_cast<AttachmentContainer *>(pSrcHnd->getContainer());
-
-    pDstContainer = 
-              dynamic_cast<AttachmentContainer *>(pDstHnd->getContainer());
-          
-    if(pSrcDesc == NULL || pDstDesc == NULL)
+    if(pSrcDesc == NULL)
     {
-        FWARNING(("Failed handle container %p %p\n", 
-                  pSrcContainer, 
-                  pDstContainer));
+        FWARNING(("subConnection: Failed to obtain field description for: "
+                  "source container [%p] field [%s]\n",
+                  pSrcContainer, szSrcName));
 
         return false;
     }

commit 80d9acf43153f7f35f9e9cda4abd73a5378d4608

fixed: field connector {add,sub}Connection node -> core lookup        (was broken by r2019)
git-svn-id: https://opensg.vrsource.org/svn/trunk@2020 4683daeb-ad0f-0410-a623-93161e962ae5

Source/Base/FieldContainer/Connector/OSGFieldConnector.cpp
@@ -89,7 +89,8 @@ bool addConnection(      OSG::AttachmentContainer *pSrcContainer,
 
             if(pSrcHnd != NULL && pSrcHnd->isValid() == true)
             {
-                pSrcDesc = pSrcHnd->getDescription();
+                pSrcContainer = pNode  ->getCore       ();
+                pSrcDesc      = pSrcHnd->getDescription();
             }
         }
     }
@@ -105,7 +106,8 @@ bool addConnection(      OSG::AttachmentContainer *pSrcContainer,
 
             if(pDstHnd != NULL && pDstHnd->isValid() == true)
             {
-                pDstDesc = pDstHnd->getDescription();
+                pDstContainer = pNode  ->getCore       ();
+                pDstDesc      = pDstHnd->getDescription();
             }
         }
     }
@@ -124,12 +126,6 @@ bool addConnection(      OSG::AttachmentContainer *pSrcContainer,
     const Field *pSrcField = pSrcHnd->getField();
           Field *pDstField = const_cast<Field *>(pDstHnd->getField());
 
-    pSrcContainer = 
-              dynamic_cast<AttachmentContainer *>(pSrcHnd->getContainer());
-
-    pDstContainer = 
-              dynamic_cast<AttachmentContainer *>(pDstHnd->getContainer());
-          
     if(pSrcContainer == NULL || pDstContainer == NULL)
     {
         FWARNING(("addConnection: Failed to obtain field handles for "
@@ -192,7 +188,8 @@ bool subConnection(      OSG::AttachmentContainer *pSrcContainer,
 
                 if(pSrcHnd != NULL && pSrcHnd->isValid() == true)
                 {
-                    pSrcDesc = pSrcHnd->getDescription();
+                    pSrcContainer = pNode  ->getCore       ();
+                    pSrcDesc      = pSrcHnd->getDescription();
                 }
             }
         }
@@ -223,7 +220,8 @@ bool subConnection(      OSG::AttachmentContainer *pSrcContainer,
 
                 if(pDstHnd != NULL && pDstHnd->isValid() == true)
                 {
-                    pDstDesc = pDstHnd->getDescription();
+                    pDstContainer = pNode  ->getCore       ();
+                    pDstDesc      = pDstHnd->getDescription();
                 }
             }
         }