18d101c6daa4a3180535130ad809e8b4e1b4ee44
cneumann <cneumann@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-08-13 21:47:09 +0000

    added: handle read back for cube textures
    git-svn-id: https://opensg.vrsource.org/svn/trunk@2026 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/Window/FrameBufferObjects/OSGTextureBuffer.cpp
+++ Source/System/Window/FrameBufferObjects/OSGTextureBuffer.cpp
@@ -192,17 +192,62 @@ void TextureBuffer::processPreDeactivate(DrawEnv *pEnv, UInt32 index)
             
             pTexImg->set(pTexImg->getPixelFormat(),
                          pTexImg->getWidth      (),
-                         pTexImg->getHeight     ());
+                         pTexImg->getHeight     (),
+                         pTexImg->getDepth      (),
+                         pTexImg->getMipMapCount(),
+                         pTexImg->getFrameCount (),
+                         pTexImg->getFrameDelay (),
+                         NULL,
+                         pTexImg->getDataType   (),
+                         true,
+                         pTexImg->getSideCount  () );
         }
-        
+
+        UInt32  mipMapLevel = _sfLevel.getValue();
+        UInt32  frame       = 0;
+        UInt32  side        = 0;
+        GLenum  target;
+        Window *pWindow = pEnv->getWindow();
+
+        if(_sfTexTarget.getValue() != GL_NONE)
+        {
+            target = _sfTexTarget.getValue();
+        }
+        else
+        {
+            target = _sfTexture.getValue()->determineTextureTarget(pWindow);
+        }
+
+        switch(target)
+        {
+        case GL_TEXTURE_CUBE_MAP_POSITIVE_X_ARB:
+            side = 0;
+            break;
+        case GL_TEXTURE_CUBE_MAP_NEGATIVE_X_ARB:
+            side = 1;
+            break;
+        case GL_TEXTURE_CUBE_MAP_POSITIVE_Y_ARB:
+            side = 2;
+            break;
+        case GL_TEXTURE_CUBE_MAP_NEGATIVE_Y_ARB:
+            side = 3;
+            break;
+        case GL_TEXTURE_CUBE_MAP_POSITIVE_Z_ARB:
+            side = 4;
+            break;
+        case GL_TEXTURE_CUBE_MAP_NEGATIVE_Z_ARB:
+            side = 5;
+            break;
+        }
+
         // select GL_COLORATTACHMENTn and read data into image
         glReadBuffer(index);
         glReadPixels(0, 0, 
-                     pTexImg->getWidth      (), 
+                     pTexImg->getWidth      (),
                      pTexImg->getHeight     (),
-                     pTexImg->getPixelFormat(), 
+                     pTexImg->getPixelFormat(),
                      pTexImg->getDataType   (),
-                     pTexImg->editData      ());
+                     pTexImg->editData      (mipMapLevel, frame, side));
 
         glReadBuffer(GL_NONE);
     }
