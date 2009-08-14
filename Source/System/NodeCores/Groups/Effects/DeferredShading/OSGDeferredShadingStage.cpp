df64783e2879c5239bd924b6704d86173332cceb
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-08-14 05:20:35 +0000

    Revert "fixed: better way, don't rely on side effects"
    This reverts commit 50fb69051079aba0460594231b18b9b415cf1843.
    
    Have to check why it does not work
    
    git-svn-id: https://opensg.vrsource.org/svn/trunk@2038 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/NodeCores/Groups/Effects/DeferredShading/OSGDeferredShadingStage.cpp
+++ Source/System/NodeCores/Groups/Effects/DeferredShading/OSGDeferredShadingStage.cpp
@@ -352,10 +352,10 @@ void DeferredShadingStage::updateStageData(
                         LightsFieldMask          )) != 0)
     {
         // copy ambient and light programs
-        DSStageData::MFShadingProgramChunksType::iterator spcIt  =
-            data->editMFShadingProgramChunks()->begin_nc();
-        DSStageData::MFShadingProgramChunksType::const spcEnd =
-            data->editMFShadingProgramChunks()->end_nc  ();
+        DSStageData::MFShadingProgramChunksType::const_iterator spcIt  =
+            data->editMFShadingProgramChunks()->begin();
+        DSStageData::MFShadingProgramChunksType::const_iterator spcEnd =
+            data->editMFShadingProgramChunks()->end  ();
 
         for(UInt32 progIdx = 0; spcIt != spcEnd; ++spcIt, ++progIdx)
         {
@@ -363,7 +363,9 @@ void DeferredShadingStage::updateStageData(
             {
                 ShaderProgramChunkUnrecPtr newSPC =
                     ShaderProgramChunk::createLocal();
-                *spcIt = newSPC;
+                //*spcIt = newSPC;
+
+                data->editMFShadingProgramChunks()->replace(progIdx, newSPC);
             }
 
             (*spcIt)->clearVertexShaders  ();
@@ -396,34 +398,36 @@ void DeferredShadingStage::updateStageData(
         }
 
         // create light chunks
-        DSStageData::MFLightChunksType::iterator lcIt  =
-            data->editMFLightChunks()->begin_nc();
-        DSStageData::MFLightChunksType::iterator lcEnd =
-            data->editMFLightChunks()->end_nc  ();
+        DSStageData::MFLightChunksType::const_iterator lcIt  =
+            data->editMFLightChunks()->begin();
+        DSStageData::MFLightChunksType::const_iterator lcEnd =
+            data->editMFLightChunks()->end  ();
 
         for(UInt32 lightIdx = 0; lcIt != lcEnd; ++lcIt, ++lightIdx)
         {
             if(*lcIt == NULL)
             {
                 DSLightChunkUnrecPtr newLC = DSLightChunk::createLocal();
-                *lcIt = newLC;
+                //*lcIt = newLC;
+                data->editMFLightChunks()->replace(lightIdx, newLC);
             }
 
             updateLightChunk(*lcIt, getLights(lightIdx));
         }
 
         // populate shading states
-        DSStageData::MFShadingStatesType::iterator stateIt  =
-            data->editMFShadingStates()->begin_nc();
-        DSStageData::MFShadingStatesType::iterator stateEnd =
-            data->editMFShadingStates()->end_nc  ();
+        DSStageData::MFShadingStatesType::const_iterator stateIt  =
+            data->editMFShadingStates()->begin();
+        DSStageData::MFShadingStatesType::const_iterator stateEnd =
+            data->editMFShadingStates()->end  ();
 
         for(UInt32 stateIdx = 0; stateIt != stateEnd; ++stateIt, ++stateIdx)
         {
             if(*stateIt == NULL)
             {
                 StateUnrecPtr newState = State::createLocal();
-                *stateIt = newState;
+                //*stateIt = newState;
+                data->editMFShadingStates()->replace(stateIdx, newState);
             }
 
             // remove all chunks
