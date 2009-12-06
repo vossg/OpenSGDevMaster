018561270e00a61b6eb3c8bce58cec8c06f06b42
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2009-12-06 08:58:03 +0000

    fixed: dependent unix build
    git-svn-id: https://opensg.vrsource.org/svn/trunk@2231 4683daeb-ad0f-0410-a623-93161e962ae5

--- Notes.cmake
+++ Notes.cmake
@@ -1,5 +1,5 @@
 
 # setup dependend build dir :
 
-cmake -DOSG_BUILD_DEPENDEND:INTERNAL=true -DOSG_REFERENCE_DIR:INTERNAL=../OpenSG.build ../OpenSG
+cmake -DOSG_BUILD_DEPENDEND:INTERNAL=true -DOSG_REFERENCE_DIR:INTERNAL=../OpenSG.build -DCMAKE_BUILD_TYPE:STRING=Release ../OpenSG
 
