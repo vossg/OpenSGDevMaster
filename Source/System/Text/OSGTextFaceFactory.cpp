d07554da998b5edad24b24bf5f3700619300c79c
cneumann <cneumann@4683daeb-ad0f-0410-a623-93161e962ae5>
2010-03-15 15:08:53 +0000

    fixed: destroy TextFaceFactory before FC Factory
           Thanks to Anreas Halm for the bug report.
    
    git-svn-id: https://external.lite3d.com/opensg/svn_repo/trunk@2324 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/Text/OSGTextFaceFactory.cpp
+++ Source/System/Text/OSGTextFaceFactory.cpp
@@ -60,7 +60,7 @@
 OSG_BEGIN_NAMESPACE
 
 
-OSG_SINGLETON_INST(TextFaceFactoryBase, addPostFactoryExitFunction)
+OSG_SINGLETON_INST(TextFaceFactoryBase, addPreFactoryExitFunction)
 
 template class SingletonHolder<TextFaceFactoryBase>;
 
