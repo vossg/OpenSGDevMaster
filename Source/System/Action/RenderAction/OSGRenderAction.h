c1a7da9cb93f5af135bccd8f98041f3ace9eb8f0
vossg <vossg@4683daeb-ad0f-0410-a623-93161e962ae5>
2010-04-11 04:04:36 +0000

    fixed: remove some dead code         Author: Carsten Neumann (from Sandbox)
    git-svn-id: https://external.lite3d.com/opensg/svn_repo/trunk@2374 4683daeb-ad0f-0410-a623-93161e962ae5

--- Source/System/Action/RenderAction/OSGRenderAction.h
+++ Source/System/Action/RenderAction/OSGRenderAction.h
@@ -380,14 +380,6 @@ class OSG_SYSTEM_DLLMAPPING RenderAction : public RenderActionBase
 //            void dump(DrawTreeNode *pRoot, UInt32 uiIndent);
 //    virtual void draw(DrawTreeNode *pRoot);
 
-#if 0
-    ResultE recurseNoNodeCallbacks(Node * const node);
-
-    ResultE recurseMultiCoreFrom(Node      * const pNode,
-                                 MultiCore * const pCore,
-                                 NodeCore  * const pFrom);
-#endif
-
   private:
 
     //-----------------------------------------------------------------------
