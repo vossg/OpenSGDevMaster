pjcamp/OpenSGDevMaster_Toolbox

Added: Method to MatrixUtility for creating a skew matrix.

Source/Base/Base/OSGMatrixUtility.h
@@ -185,6 +185,14 @@ OSG_BASE_DLLMAPPING void setViewMatEyePos       (      Matrix &Result,
 /*! \} 
  */
 
+/*! \ingroup GrpBaseBaseMathMatrixFn
+    \relatesalso TransformationMatrix
+ */
+OSG_BASE_DLLMAPPING void MatrixSkew               (      Matrix &Result,
+                                                 const Vec3f  &RotationAxis,
+                                                 const Vec3f  &TranslationAxis,
+                                                 Real32 AngleInRadians);
+
 OSG_END_NAMESPACE
 
 #endif /* _OSGMATRIXUTILITY_H_ */

vossg/OpenSGDevMaster

Signed-off-with-cleanup-by: gerrit <vossg@vossg.org>

Source/Base/Base/OSGMatrixUtility.h
@@ -137,6 +137,14 @@ OSG_BASE_DLLMAPPING bool MatrixProjection       (OSG::Matrix &result,
 /*! \ingroup GrpBaseBaseMathMatrixFn
     \relatesalso TransformationMatrix
  */
+OSG_BASE_DLLMAPPING void MatrixSkew             (      Matrix &result,
+                                                 const Vec3f  &vRotationAxis,
+                                                 const Vec3f  &vTranslationAxis,
+                                                       Real32  rAngleInRadians);
+
+/*! \ingroup GrpBaseBaseMathMatrixFn
+    \relatesalso TransformationMatrix
+ */
 OSG_BASE_DLLMAPPING void makeViewFromUVND       (      Matrix &Result,
                                                  const Vec3f  &U,
                                                  const Vec3f  &V,