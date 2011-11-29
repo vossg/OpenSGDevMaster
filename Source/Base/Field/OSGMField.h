pjcamp/OpenSGDevMaster_Toolbox

Added: A function to MFields to allow inserting values using an index instead of an iterator.

Source/Base/Field/OSGMField.h
@@ -215,6 +215,8 @@ class MField : public Field
     
     void                   clear    (void                              );
     
+    iterator               insert   (size_type    pos,
+                                     ArgumentType value                );
     iterator               insert   (iterator     pos,
                                      ArgumentType value                );
     void                   insert   (iterator     pos,

vossg/OpenSGDevMaster

Signed-off-with-cleanup-by: gerrit <vossg@vossg.org>

Source/Base/Field/OSGMField.h
@@ -219,6 +219,8 @@ class MField : public Field
     
     void                   clear    (void                              );
     
+    iterator               insert   (size_type    index,
+                                     ArgumentType value                );
     iterator               insert   (iterator     pos,
                                      ArgumentType value                );
     void                   insert   (iterator     pos,