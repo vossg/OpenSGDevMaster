5706b559726d91f286e74b0c5ef1cc9774e4be4b
gerrit <vossg@vossg.org>
2011-12-19 13:26:57 +0800

    added: 14clustering stream socket variant

---
+++ Examples/Tutorial/14clusteringStream_Client.cpp
@@ -0,0 +1,115 @@
+// all needed include files
+#ifdef OSG_BUILD_ACTIVE
+#include <OSGGLUT.h>
+#include <OSGConfig.h>
+#include <OSGSimpleGeometry.h>
+#include <OSGGLUTWindow.h>
+#include <OSGSimpleSceneManager.h>
+
+#include <OSGMultiDisplayWindow.h>
+#include <OSGSceneFileHandler.h>
+#else
+#include <OpenSG/OSGGLUT.h>
+#include <OpenSG/OSGConfig.h>
+#include <OpenSG/OSGSimpleGeometry.h>
+#include <OpenSG/OSGGLUTWindow.h>
+#include <OpenSG/OSGSimpleSceneManager.h>
+
+#include <OpenSG/OSGMultiDisplayWindow.h>
+#include <OpenSG/OSGSceneFileHandler.h>
+#endif
+
+OSG::SimpleSceneManager *mgr;
+OSG::NodeRecPtr          scene;
+
+int setupGLUT(int *argc, char *argv[]);
+
+int main(int argc, char **argv)
+{
+    OSG::ChangeList::setReadWriteDefault(true);
+    OSG::osgInit(argc,argv);
+
+    {
+        setupGLUT(&argc, argv);
+        
+        //this time we'll have not a GLUTWindow here, but this one
+        OSG::MultiDisplayWindowRecPtr multiWindow = 
+            OSG::MultiDisplayWindow::create();
+        
+        //set some necessary values
+        // we connect via multicast
+        multiWindow->setConnectionType("StreamSock");
+        //multiWindow->setServiceAddress("192.168.2.142");
+        // we want to rendering servers... 
+        multiWindow->editMFServers()->push_back("Server1");
+        multiWindow->editMFServers()->push_back("Server2");	
+
+        //any scene here
+        scene = OSG::makeTorus(.5, 2, 16, 16);
+        
+        // and the ssm as always
+        mgr = new OSG::SimpleSceneManager;
+        
+        mgr->setWindow(multiWindow);
+        mgr->setRoot  (scene);
+        mgr->showAll();
+        
+        multiWindow->init();
+        multiWindow->resize(512, 512);
+
+        OSG::commitChanges();
+    }
+    
+    glutMainLoop();
+    
+    return 0;
+}
+
+void display(void)
+{
+    //redrawing as usual
+    mgr->redraw();
+    
+    //the changelist should be cleared - else things
+    //could be copied multiple times
+    OSG::Thread::getCurrentChangeList()->clear();
+    
+    // to ensure a black navigation window
+    glClear(GL_COLOR_BUFFER_BIT);
+    glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+    glutPostRedisplay();
+}
+
+void mouse(int button, int state, int x, int y)
+{
+    if (state)
+        mgr->mouseButtonRelease(button, x, y);
+    else
+        mgr->mouseButtonPress(button, x, y);
+    glutPostRedisplay();
+}
+
+void motion(int x, int y)
+{
+    mgr->mouseMove(x, y);
+    glutPostRedisplay();
+}
+
+int setupGLUT(int *argc, char *argv[])
+{
+    glutInit(argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+    
+    int winid = glutCreateWindow("OpenSG");
+    
+    glutReshapeFunc(reshape);
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    
+    return winid;
+}
---
+++ Examples/Tutorial/14clusteringStream_Server.cpp
@@ -0,0 +1,74 @@
+#ifdef OSG_BUILD_ACTIVE
+#include <OSGGLUT.h>
+#include <OSGConfig.h>
+#include <OSGClusterServer.h>
+#include <OSGGLUTWindow.h>
+#include <OSGRenderAction.h>
+#else
+#include <OpenSG/OSGGLUT.h>
+#include <OpenSG/OSGConfig.h>
+#include <OpenSG/OSGClusterServer.h>
+#include <OpenSG/OSGGLUTWindow.h>
+#include <OpenSG/OSGRenderAction.h>
+#endif
+
+OSG::GLUTWindowRecPtr   window;
+OSG::RenderAction      *ract;
+OSG::ClusterServer     *server;
+
+void display(void);
+void reshape(int width, int height);
+
+int main(int argc,char **argv)
+{
+    int winid;
+
+    // initialize Glut
+    glutInit(&argc, argv);
+    glutInitDisplayMode( GLUT_RGB |GLUT_DEPTH | GLUT_DOUBLE);
+
+    if(!argv[1])
+    {
+        std::cout << "No name was given!" << std::endl;
+        return -1;
+    }
+    
+    // init OpenSG
+    OSG::osgInit(argc, argv);
+
+    winid = glutCreateWindow(argv[1]);
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+    glutReshapeFunc(reshape);
+    glutSetCursor(GLUT_CURSOR_NONE);
+
+    ract = OSG::RenderAction::create();
+
+    window = OSG::GLUTWindow::create();
+    window->setGlutId(winid);
+    window->init();
+
+    window->resize(512, 512);
+
+    //create a new server that will be connected via multicast
+    //argv[1] is the name of the server (at least it should be...)
+    server = new OSG::ClusterServer(window, argv[1], "StreamSock", "");
+    server->start();
+
+    glutMainLoop();
+
+    return 0;
+}
+
+void display(void)
+{
+    //simply execute the render action
+    server->render(ract);
+    //and delete the change list
+    OSG::Thread::getCurrentChangeList()->clear();
+}
+
+void reshape(int width, int height)
+{
+    window->resize(width, height);
+}
